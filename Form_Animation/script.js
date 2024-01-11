3d2517a2d2af97ca49eb9ddcdac5b1141058b147
Frantisek Pelech <feromiro23@gmail.com>
2024-01-11 12:18:01 +0100

    used style transition delay for animation in script.js .map

--- Form_Animation/script.js
+++ Form_Animation/script.js
@@ -3,6 +3,9 @@ console.log(label[0].textContent);
 label.forEach((element) => {
   element.innerHTML = element.innerText
     .split("")
-    .map((letter, index) => `<span>${letter}</span>`)
+    .map(
+      (letter, index) =>
+        `<span style="transition-delay:${index * 100}ms">${letter}</span>`
+    )
     .join("");
 });
