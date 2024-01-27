a5657492055a3b647778f554c50b4c69b59fa1df
Frantisek Pelech <feromiro23@gmail.com>
2024-01-27 15:31:59 +0100

    functionality for searcher

--- Movie_List/script.js
+++ Movie_List/script.js
@@ -1,8 +1,10 @@
 const API_URL =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=b9a0adfcde2ed4c342e4e675fd44429d&page=1";
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCH_URL =
+const SEARCH_API =
   'https://api.themoviedb.org/3/search/movie?api_key=b9a0adfcde2ed4c342e4e675fd44429d&query="';
+const form = document.getElementById("form");
+const search = document.getElementById("search");
 
 getMovies(API_URL);
 
@@ -11,3 +13,15 @@ async function getMovies(url) {
   const data = await res.json();
   console.log(data.results);
 }
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const searchTerm = search.value;
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_API + searchTerm);
+
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
