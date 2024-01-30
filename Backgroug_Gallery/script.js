eecb677db53ae1a91065083fde3b6418958cb423
Frantisek Pelech <feromiro23@gmail.com>
2024-01-30 11:32:25 +0100

    Background image set with js

---
+++ Backgroug_Gallery/script.js
@@ -0,0 +1,12 @@
+// background-image: url("https://images.unsplash.com/photo-1687716005788-3a31b81f70f8?q=80&w=1948&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+const body = document.body;
+const slides = document.querySelectorAll(".slide");
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
+let activeSlide = 0;
+
+setBgToBody();
+
+function setBgToBody() {
+  body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+}
