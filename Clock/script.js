9ff2c49ca8dd0987cb2e1566bebb5a621f45b71b
Frantisek Pelech <feromiro23@gmail.com>
2024-02-02 01:09:31 +0100

    Created all start constants in script.js

---
+++ Clock/script.js
@@ -0,0 +1,29 @@
+const hourEl = document.querySelector(".hour");
+const minuteEl = document.querySelector(".minute");
+const secondEl = document.querySelector(".second");
+const timeEl = document.querySelector(".time");
+const dateEl = document.querySelector(".date");
+const btnEl = document.querySelector(".btn");
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
