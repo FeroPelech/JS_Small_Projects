c1ad038a7eed42ad81f54ee792453dd944edb6d2
Frantisek Pelech <feromiro23@gmail.com>
2024-02-06 19:10:18 +0100

    All done

--- Waves_Placeholder/script.js
+++ Waves_Placeholder/script.js
@@ -8,6 +8,8 @@ const date = document.getElementById("date");
 const animatedBgs = document.querySelectorAll(".animated-bg");
 const animatedBgText = document.querySelectorAll(".animated-bg-text");
 
+setTimeout(getData, 2500);
+
 function getData() {
   header.innerHTML =
     '<img src="https://images.unsplash.com/photo-1682686581220-689c34afb6ef?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">';
@@ -18,4 +20,6 @@ function getData() {
     '<img src="https://randomuser.me/api/portraits/thumb/women/28.jpg" alt="">';
   name.innerHTML = "Janka Mrkvova";
   date.innerHTML = "Apr 02 1962";
+  animatedBgs.forEach((e) => e.classList.remove("animated-bg"));
+  animatedBgText.forEach((e) => e.classList.remove("animated-bg-text"));
 }
