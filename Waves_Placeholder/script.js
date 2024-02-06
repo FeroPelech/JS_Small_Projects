32264f298824c7cac6fa5609b5a4e4fbf0da1722
Frantisek Pelech <feromiro23@gmail.com>
2024-02-06 18:56:09 +0100

    Created all necessary constants and function getData

---
+++ Waves_Placeholder/script.js
@@ -0,0 +1,21 @@
+const header = document.getElementById("header");
+const title = document.getElementById("title");
+const except = document.getElementById("except");
+const profilImg = document.getElementById("profile_img");
+const name = document.getElementById("name");
+const date = document.getElementById("date");
+
+const animatedBgs = document.querySelectorAll(".animated-bg");
+const animatedBgText = document.querySelectorAll(".animated-bg-text");
+
+function getData() {
+  header.innerHTML =
+    '<img src="https://images.unsplash.com/photo-1682686581220-689c34afb6ef?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">';
+  title.innerHTML = "Lorem ipsum dolor sit amet";
+  except.innerHTML =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum velit";
+  profilImg.innerHTML =
+    '<img src="https://randomuser.me/api/portraits/thumb/women/28.jpg" alt="">';
+  name.innerHTML = "Janka Mrkvova";
+  date.innerHTML = "Apr 02 1962";
+}
