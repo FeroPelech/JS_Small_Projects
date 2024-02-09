8f089775e4cf9373af78a767a23c3b94e4c4036e
Frantisek Pelech <feromiro23@gmail.com>
2024-02-09 13:29:00 +0100

    Up button work

--- Slider_Gallery/script.js
+++ Slider_Gallery/script.js
@@ -5,4 +5,22 @@ const upBtn = document.querySelector(".up-btn");
 const downBtn = document.querySelector(".down-btn");
 const slideLength = slideRight.querySelectorAll("div").length;
 
-console.log(slideLength);
+let activeSlideIndex = 0;
+
+slideLeft.style.top = `-${(slideLength - 1) * 100}vh`;
+
+upBtn.addEventListener("click", () => changeSlide("up"));
+downBtn.addEventListener("click", () => changeSlide("down"));
+
+const changeSlide = (direction) => {
+  const sliderHeight = sliderContainer.clientHeight;
+  if (direction === "up") {
+    activeSlideIndex++;
+    if (activeSlideIndex > slideLength - 1) {
+      activeSlideIndex = 0;
+    }
+  }
+  slideRight.style.transform = `translateY(-${
+    activeSlideIndex * sliderHeight
+  }px)`;
+};
