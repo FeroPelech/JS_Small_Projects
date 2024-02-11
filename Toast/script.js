44d0cea7487264b28a25826f81266ffab78f95fb
Frantisek Pelech <feromiro23@gmail.com>
2024-02-11 16:49:54 +0100

    Created listener on button click and small correction in index.html

--- Toast/script.js
+++ Toast/script.js
@@ -2,3 +2,9 @@ const button = document.getElementById("button");
 const toasts = document.getElementById("toasts");
 
 const messages = ["Message", "Message Two", "Message Three", "Message Four"];
+
+button.addEventListener("click", () => createNotification());
+
+function createNotification() {
+  console.log(123);
+}
