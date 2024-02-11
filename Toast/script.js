f90a9fa46679ffe12a1ce9c7076e1987d8f762f4
Frantisek Pelech <feromiro23@gmail.com>
2024-02-11 16:56:41 +0100

    Creted functions for notification and random choice

--- Toast/script.js
+++ Toast/script.js
@@ -6,5 +6,12 @@ const messages = ["Message", "Message Two", "Message Three", "Message Four"];
 button.addEventListener("click", () => createNotification());
 
 function createNotification() {
-  console.log(123);
+  const notif = document.createElement("div");
+  notif.classList.add("toast");
+  notif.innerText = getRandomMessage();
+  toasts.appendChild(notif);
+}
+
+function getRandomMessage() {
+  return messages[Math.floor(Math.random() * messages.length)];
 }
