f293d6807faae409ef13b615c4caa46decef4d9c
Frantisek Pelech <feromiro23@gmail.com>
2024-02-12 12:21:35 +0100

    Created dynamic card info and deleted useless html code from index.html

--- Github_Users/script.js
+++ Github_Users/script.js
@@ -2,14 +2,40 @@ const APIURL = "https://api.github.com/users/";
 
 const form = document.getElementById("form");
 const search = document.getElementById("search");
+const main = document.getElementById("main");
 
 async function getUser(userName) {
   try {
     const { data } = await axios(APIURL + userName);
-    console.log(data);
+    createUserCard(data);
   } catch (err) {
     console.log(err);
   }
+}
+
+function createUserCard(user) {
+  const cardHTML = `        
+  <div class="card">
+    <div>
+      <img src="${user.avatar_url}" alt="user picture" class="avatar">
+    </div>
+    <div class="user-info">
+      <h2>${user.name}</h2>
+      <p>${user.bio}</p>
+      <ul>
+          <li>${user.followers}<strong>Followers</strong></li>
+          <li>${user.following}<strong>Following</strong></li>
+          <li>${user.public_repos}<strong>Repos</strong></li>
+      </ul>
+      <div id="repos">
+          <a href="#" class="repo">Repo 1</a>
+          <a href="#" class="repo">Repo 2</a>
+          <a href="#" class="repo">Repo 3</a>
+      </div>
+    </div>
+  </div>`;
+
+  main.innerHTML = cardHTML;
 }
 
 form.addEventListener("submit", (e) => {
