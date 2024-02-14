c67a5a80febd2a17281f2a4c0e4d0bac94ea0542
Frantisek Pelech <feromiro23@gmail.com>
2024-02-14 23:36:49 +0100

    Created Listener on class loveMe

--- Likes_Effect/script.js
+++ Likes_Effect/script.js
@@ -2,3 +2,15 @@ const loveMe = document.querySelector(".loveMe");
 const times = document.querySelector("#times");
 
 let clickTime = 0;
+
+loveMe.addEventListener("click", (e) => {
+  if (clickTime === 0) {
+    clickTime = new Date().getTime();
+  } else {
+    if (new Date().getTime() - clickTime < 800) {
+      clickTime = 0;
+    } else {
+      clickTime = new Date().getTime();
+    }
+  }
+});
