4cf36dd97715485db385b35f88377a65d0bda0af
Frantisek Pelech <feromiro23@gmail.com>
2024-02-15 14:13:13 +0100

    Deleted message from index.html and created all necessary constants in script.js

---
+++ Auto_Typing/script.js
@@ -0,0 +1,6 @@
+const text = document.getElementById("text");
+const speed = document.getElementById("speed");
+const message = "Autotyping text are comming!";
+
+let idx = 1;
+let actualSpeed = 300 / speed.value;
