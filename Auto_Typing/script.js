74b6ddf12aac721ccc30ce1858caa8333127016b
Frantisek Pelech <feromiro23@gmail.com>
2024-02-15 14:23:17 +0100

    Created listener for input speed

--- Auto_Typing/script.js
+++ Auto_Typing/script.js
@@ -4,3 +4,17 @@ const message = "Autotyping text are comming!";
 
 let idx = 1;
 let actualSpeed = 300 / speed.value;
+
+writeText();
+
+function writeText() {
+  text.innerText = message.slice(0, idx);
+  idx++;
+  if (idx > message.length) {
+    idx = 1;
+  }
+
+  setTimeout(writeText, actualSpeed);
+}
+
+speed.addEventListener("input", (e) => (actualSpeed = 300 / e.target.value));
