ff00f144baf71368032d4b07de1440af59791307
Frantisek Pelech <feromiro23@gmail.com>
2024-02-20 19:55:49 +0100

    Created doContent function for listener and small corrections

--- Checkbox/script.js
+++ Checkbox/script.js
@@ -1,4 +1,12 @@
 const toggles = document.querySelectorAll(".toggle");
-const fly = document.querySelector(".fly");
-const power = document.querySelector(".power");
-const wisdom = document.querySelector(".wisdom");
+const fly = document.getElementById("fly");
+const power = document.getElementById("power");
+const wisdom = document.getElementById("wisdom");
+
+toggles.forEach((toggle) =>
+  toggle.addEventListener("change", (e) => doContent(e.target))
+);
+
+function doContent(part) {
+  console.log(part);
+}
