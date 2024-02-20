ee14343d3eb53bee598a4d2af9749f6332562578
Frantisek Pelech <feromiro23@gmail.com>
2024-02-20 19:50:31 +0100

    Created all constants

---
+++ Checkbox/script.js
@@ -0,0 +1,4 @@
+const toggles = document.querySelectorAll(".toggle");
+const fly = document.querySelector(".fly");
+const power = document.querySelector(".power");
+const wisdom = document.querySelector(".wisdom");
