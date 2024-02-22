5154e830fcca5261456ed9855ff1f0aeb50947c7
Frantisek Pelech <feromiro23@gmail.com>
2024-02-22 16:26:21 +0100

    Problem with error and unactive textarea solved

--- Notes_App/script.js
+++ Notes_App/script.js
@@ -3,23 +3,32 @@ addBtn.addEventListener("click", () => addNewNote());
 
 function addNewNote(text = "") {
   const note = document.createElement("div");
+  console.log(note);
   note.classList.add("note");
-
+  console.log(note);
   note.innerHTML = `
-    <div class="tools">
-        <button class="edit"><i class="fas fa-edit"></i></button>
-        <button class="delete"><i class="fas fa-trash-alt"></i></button>
-    </div>
-    <div class="main ${text ? "" : "hidden"}></div>
-    <textarea class="${text ? "hidden" : ""}"></textarea>
-    `;
+        <div class="tools">
+            <button class="edit"><i class="fas fa-edit"></i></button>
+            <button class="delete"><i class="fas fa-trash-alt"></i></button>
+        </div>
+        <div class="main ${text ? " " : "hidden"}"></div>
+        <textarea class= "${text ? "hidden" : " "}"></textarea>`;
   const editBtn = note.querySelector(".edit");
   const deleteBtn = note.querySelector(".delete");
   const main = note.querySelector(".main");
   const textArea = note.querySelector("textarea");
 
+  textArea.value = text;
+  console.log(textArea.value);
+  main.innerHTML = marked(text);
+
   deleteBtn.addEventListener("click", () => {
     note.remove();
+  });
+
+  editBtn.addEventListener("click", () => {
+    main.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
   });
   document.body.appendChild(note);
 }
