39c1a3dde2af8f84bbae2f2eb535e042cda51c1d
Frantisek Pelech <feromiro23@gmail.com>
2024-02-23 01:44:48 +0100

    Created local storage and update function

--- Notes_App/script.js
+++ Notes_App/script.js
@@ -1,11 +1,15 @@
 const addBtn = document.getElementById("add");
+const notes = JSON.parse(localStorage.getItem("notes"));
+
+if (notes) {
+  notes.forEach((note) => addNewNote(note));
+}
+
 addBtn.addEventListener("click", () => addNewNote());
 
 function addNewNote(text = "") {
   const note = document.createElement("div");
-  console.log(note);
   note.classList.add("note");
-  console.log(note);
   note.innerHTML = `
         <div class="tools">
             <button class="edit"><i class="fas fa-edit"></i></button>
@@ -19,11 +23,11 @@ function addNewNote(text = "") {
   const textArea = note.querySelector("textarea");
 
   textArea.value = text;
-  console.log(textArea.value);
   main.innerHTML = marked(text);
 
   deleteBtn.addEventListener("click", () => {
     note.remove();
+    updateLS();
   });
 
   editBtn.addEventListener("click", () => {
@@ -34,6 +38,14 @@ function addNewNote(text = "") {
   textArea.addEventListener("input", (e) => {
     const { value } = e.target;
     main.innerHTML = marked(value);
+    updateLS();
   });
   document.body.appendChild(note);
+}
+
+function updateLS() {
+  const notesText = document.querySelectorAll("textarea");
+  const notes = [];
+  notesText.forEach((note) => notes.push(note.value));
+  localStorage.setItem("notes", JSON.stringify(notes));
 }
