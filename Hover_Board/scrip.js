1efc605a6701eafa4cb758da43d18418017cdcfc
Frantisek Pelech <feromiro23@gmail.com>
2024-03-02 23:41:30 +0100

    Created style for color change square

--- Hover_Board/scrip.js
+++ Hover_Board/scrip.js
@@ -1,23 +1,26 @@
 const container = document.getElementById("container");
 const colors = ["#e74c3c", "#8e44ad", "#3498db", "#e67e22", "#2ecc71"];
 
-const squares = 500;
+const SQUARES = 500;
 
-for (let i = 0; i < squares; i++) {
+for (let i = 0; i < SQUARES; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
+
   square.addEventListener("mouseover", () => {
     setColor(square);
   });
   square.addEventListener("mouseout", () => {
     removeColor(square);
   });
+
   container.appendChild(square);
 }
 
 function setColor(e) {
   const color = getRandomColor();
-  console.log(e);
+  e.style.background = color;
+  e.style.boxShadow = `0 0 5px ${color}, 0 0 50px ${color}`;
 }
 
 function removeColor(e) {}
