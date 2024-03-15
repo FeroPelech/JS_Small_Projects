094976e2c9e9f7d7e38569e94ed8a8f9b171bad9
Frantisek Pelech <feromiro23@gmail.com>
2024-03-15 19:31:56 +0100

    Created foreach loop

--- Mobile_Navbar/script.js
+++ Mobile_Navbar/script.js
@@ -1,2 +1,9 @@
 const contents = document.querySelectorAll(".content");
 const listItems = document.querySelectorAll("nav li ul");
+
+listItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    hideAllContents();
+    hideAllItems();
+  });
+});
