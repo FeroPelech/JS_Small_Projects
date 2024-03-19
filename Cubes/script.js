dc0b4b57b317f417b14c4710d8688cb7b6998b3e
Frantisek Pelech <feromiro23@gmail.com>
2024-03-19 01:12:43 +0100

    Created listener for final move

--- Cubes/script.js
+++ Cubes/script.js
@@ -1,6 +1,8 @@
 const bexesContainer = document.getElementById("boxes");
 const btn = document.getElementById("btn");
 
+btn.addEventListener("click", () => bexesContainer.classList.toggle("big"));
+
 function createBoxes() {
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
