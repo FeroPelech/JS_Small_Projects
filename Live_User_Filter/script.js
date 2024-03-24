0e31e94bd385c90c510cc17bb9111223b2e81471
Frantisek Pelech <feromiro23@gmail.com>
2024-03-24 20:52:05 +0100

    Created async function getData for fetch users

--- Live_User_Filter/script.js
+++ Live_User_Filter/script.js
@@ -1,3 +1,11 @@
 const result = document.getElementById("result");
 const filter = document.getElementById("filter");
 const listItems = [];
+
+async function getData() {
+  const res = await fetch("https://randomuser.me/api?results=50");
+  const data = await res.json();
+  console.log(data);
+}
+
+getData();
