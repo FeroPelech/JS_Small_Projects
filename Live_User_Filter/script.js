281bef1885202ad545f57f213115d9ebab0e2557
Frantisek Pelech <feromiro23@gmail.com>
2024-03-25 19:55:50 +0100

    Created list of peoples

--- Live_User_Filter/script.js
+++ Live_User_Filter/script.js
@@ -2,14 +2,23 @@ const result = document.getElementById("result");
 const filter = document.getElementById("filter");
 const listItems = [];
 
-async function getData() {
-  const res = await fetch("https://randomuser.me/api?results=50");
-  const { results } = await res.json();
+async function getDuta() {
+  const name = await fetch("https://randomuser.me/api?results=50");
+  const datafromfetch = await name.json();
 
-  results.innerHTML = "";
-  results.forEach((user) => {
-    console.log(user);
+  datafromfetch.results.forEach((element) => {
+    const li = document.createElement("li");
+    listItems.push(li);
+    li.innerHTML = `
+      <img src="${element.picture.large}" alt="${element.name.first}">
+      <div class="user-info">
+        <h4>"${element.name.first} ${element.name.last}"</h4>
+        <p>"${element.location.city}, ${element.location.country}"</p>
+      </div>
+      `;
+
+    result.appendChild(li);
   });
 }
 
-getData();
+getDuta();
