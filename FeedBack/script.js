5992d0a44cfcd442a24f1dad00ca904f7198d91e
Frantisek Pelech <feromiro23@gmail.com>
2024-03-26 18:38:54 +0100

    Function remove active for remove class active from rating

--- FeedBack/script.js
+++ FeedBack/script.js
@@ -2,6 +2,15 @@ const ratings = document.querySelectorAll(".rating");
 const sendBtn = document.querySelector(".btn");
 const panel = document.getElementById("panel");
 
-panel.addEventListener("click", () => {
-  console.log("click");
+panel.addEventListener("click", (e) => {
+  if (e.target.parentNode.classList.contains("rating")) {
+    removeActive();
+    e.target.parentNode.classList.add("active");
+  }
 });
+
+function removeActive() {
+  ratings.forEach((e) => {
+    e.classList.remove("active");
+  });
+}
