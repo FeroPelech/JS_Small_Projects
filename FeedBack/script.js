fb9db2175a006c1328f66d762d0cb271963deb43
Frantisek Pelech <feromiro23@gmail.com>
2024-03-26 21:57:20 +0100

    Created content for button action after push

--- FeedBack/script.js
+++ FeedBack/script.js
@@ -1,12 +1,25 @@
 const ratings = document.querySelectorAll(".rating");
+const ratingsContainer = document.querySelector(".ratings-container");
 const sendBtn = document.querySelector(".btn");
 const panel = document.getElementById("panel");
+let selectedEmoji = "Honey!";
 
-panel.addEventListener("click", (e) => {
+ratingsContainer.addEventListener("click", (e) => {
   if (e.target.parentNode.classList.contains("rating")) {
     removeActive();
     e.target.parentNode.classList.add("active");
+    selectedEmoji = e.target.nextElementSibling.innerHTML;
   }
+});
+
+sendBtn.addEventListener("click", (e) => {
+  panel.innerHTML = `
+    <i class="fas fa-heart"></i>
+    <strong>Thank You!</strong>
+    <br>
+    <strong>Feedback: ${selectedEmoji}</strong>
+    <p>We'll use your feedback to improve our special services</p>
+    `;
 });
 
 function removeActive() {
