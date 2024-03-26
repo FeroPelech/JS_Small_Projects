86afc64695994b691b651b3cda8133f265ee94c2
Frantisek Pelech <feromiro23@gmail.com>
2024-03-26 17:01:58 +0100

    Created 3 constants and check panel class with listener

---
+++ FeedBack/script.js
@@ -0,0 +1,7 @@
+const ratings = document.querySelectorAll(".rating");
+const sendBtn = document.querySelector(".btn");
+const panel = document.getElementById("panel");
+
+panel.addEventListener("click", () => {
+  console.log("click");
+});
