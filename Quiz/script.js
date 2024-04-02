763f95c54a461e7b4331d8a0dec1d202f0ef2a36
Frantisek Pelech <feromiro23@gmail.com>
2024-04-03 00:08:51 +0200

    Inserted all options

--- Quiz/script.js
+++ Quiz/script.js
@@ -50,4 +50,8 @@ loadQuiz();
 function loadQuiz() {
   const currentQuizData = quizQuestions[currentQuiz];
   questionEl.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
 }
