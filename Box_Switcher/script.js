8f3fd5bd6ffcae4618ce0a6dff3363f997340633
Frantisek Pelech <feromiro23@gmail.com>
2024-04-07 20:54:08 +0200

    Preparation for content

--- Box_Switcher/script.js
+++ Box_Switcher/script.js
@@ -3,3 +3,15 @@ const testimonial = document.querySelector(".testimonial");
 const userImage = document.querySelector(".user-image");
 const userName = document.querySelector(".username");
 const role = document.querySelector(".role");
+
+const testimonials = [
+  {
+    name: "Trevor Barack",
+    position: "GirlHunter",
+    photo: "https://randomuser.me/api/portraits/men/45.jpg",
+    text: `You know the scenario. It could be in an online meeting, or <br>
+        perhaps you are seated around a boardroom table. The meeting <br>
+        leader asks everyone to briefly introduce themselves.<br>
+        Suddenly, your brain goes into hyperdrive. What should I say about myself?<br>`,
+  },
+];
