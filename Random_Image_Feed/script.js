e7ab4c479adb667c39ecb7035ed8c92ca03a71ef
Frantisek Pelech <feromiro23@gmail.com>
2024-04-09 19:31:52 +0200

    Created for loop

--- Random_Image_Feed/script.js
+++ Random_Image_Feed/script.js
@@ -1,3 +1,8 @@
 const container = document.querySelector(".container");
 const unsplashPics = "https://source.unsplash.com/random";
-const row = 10;
+const rows = 10;
+
+for (let i = 0; i < rows * 3; i++) {
+  const img = document.createElement("img");
+  img.src = `${unsplashPics}`;
+}
