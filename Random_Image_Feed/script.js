5e4464c0ba14fe84fd526f41dc80f122a378ce44
Frantisek Pelech <feromiro23@gmail.com>
2024-04-10 21:06:27 +0200

    Created condition to stop using same picture

--- Random_Image_Feed/script.js
+++ Random_Image_Feed/script.js
@@ -1,11 +1,19 @@
 const container = document.querySelector(".container");
 const unsplashPics = "https://source.unsplash.com/random/";
-const rows = 10;
+const rows = 5;
+const imageList = [];
 
 for (let i = 0; i < rows * 3; i++) {
   const img = document.createElement("img");
-  img.src = `${unsplashPics}+${getRandomSize()}`;
-  container.appendChild(img);
+  const randomSize = getRandomSize();
+
+  if (!imageList.includes(randomSize)) {
+    imageList.push(randomSize);
+    img.src = `${unsplashPics}+${imageList[i]}`;
+    container.appendChild(img);
+    i++;
+  }
+  i--;
 }
 
 function getRandomNumber() {
@@ -15,5 +23,3 @@ function getRandomNumber() {
 function getRandomSize() {
   return `${getRandomNumber()}x${getRandomNumber()}`;
 }
-
-console.log(getRandomSize());
