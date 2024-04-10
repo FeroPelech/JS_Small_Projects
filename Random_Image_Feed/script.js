c51624c3525debf9dfd3c0224a3dc06bd15bf8a2
Frantisek Pelech <feromiro23@gmail.com>
2024-04-10 20:32:06 +0200

    Small correction in unsplashPics

--- Random_Image_Feed/script.js
+++ Random_Image_Feed/script.js
@@ -1,10 +1,11 @@
 const container = document.querySelector(".container");
-const unsplashPics = "https://source.unsplash.com/random";
+const unsplashPics = "https://source.unsplash.com/random/";
 const rows = 10;
 
 for (let i = 0; i < rows * 3; i++) {
   const img = document.createElement("img");
-  img.src = `${unsplashPics}`;
+  img.src = `${unsplashPics}+${getRandomSize()}`;
+  container.appendChild(img);
 }
 
 function getRandomNumber() {
