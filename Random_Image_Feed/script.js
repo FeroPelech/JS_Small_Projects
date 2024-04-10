9607f9871527d20455d115a3f30065a3f64c92f8
Frantisek Pelech <feromiro23@gmail.com>
2024-04-10 20:24:16 +0200

    Created function getRandomNumber

--- Random_Image_Feed/script.js
+++ Random_Image_Feed/script.js
@@ -6,3 +6,9 @@ for (let i = 0; i < rows * 3; i++) {
   const img = document.createElement("img");
   img.src = `${unsplashPics}`;
 }
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 10) + 300;
+}
+
+console.log(getRandomNumber());
