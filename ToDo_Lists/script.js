a204d7091395aaf0d43c9cca71741b92bb130f25
Frantisek Pelech <feromiro23@gmail.com>
2024-04-16 00:46:36 +0200

    Created addTodo function

--- ToDo_Lists/script.js
+++ ToDo_Lists/script.js
@@ -1,3 +1,29 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const todoUL = document.getElementById("todos");
+
+function addTodo(todo) {
+  let todoText = input.value;
+  //   console.log(todoText);
+  if (todo) {
+    todoText = todo.text;
+  }
+
+  if (todoText) {
+    const todoEl = document.createElement("li");
+    if (todo && todo.completed) {
+      todoEl.classList.add("completed");
+    }
+    todoEl.innerText = todoText;
+    todoEl.addEventListener("click", () => {
+      todoEl.classList.toggle("completed");
+    });
+    todoEl.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      todoEl.remove();
+    });
+
+    todoUL.appendChild(todoEl);
+    input.value = "";
+  }
+}
