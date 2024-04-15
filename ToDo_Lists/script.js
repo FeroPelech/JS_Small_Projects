8e632844fdfce38be3178fa02d85e9a932d447eb
Frantisek Pelech <feromiro23@gmail.com>
2024-04-16 00:48:10 +0200

    Created listener on submit event

--- ToDo_Lists/script.js
+++ ToDo_Lists/script.js
@@ -2,6 +2,11 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const todoUL = document.getElementById("todos");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addTodo();
+});
+
 function addTodo(todo) {
   let todoText = input.value;
   //   console.log(todoText);
