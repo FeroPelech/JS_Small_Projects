7a3ada943979df0f7d741003eb31af6a67b19615
Frantisek Pelech <feromiro23@gmail.com>
2024-04-22 10:16:05 +0200

    Created function getRandomLocation for show insects on differenr places

--- Catch_Game/script.js
+++ Catch_Game/script.js
@@ -30,3 +30,11 @@ function createInsect() {
   insect.classList.add("insect");
   const { x, y } = getRandomLocation();
 }
+
+function getRandomLocation() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const x = Math.random() * (width - 200) + 100;
+  const y = Math.random() * (height - 200) + 100;
+  return { x, y };
+}
