fc99b03ce2e69ad1e11c8d720c7a916f91067414
Frantisek Pelech <feromiro23@gmail.com>
2024-04-22 09:55:50 +0200

    Created other variables

--- Catch_Game/script.js
+++ Catch_Game/script.js
@@ -5,3 +5,6 @@ const timeEl = document.getElementById("time");
 const scoreEl = document.getElementById("score");
 const message = document.getElementById("message");
 const game_container = document.getElementById("game-container");
+let seconds = 0;
+let score = 0;
+let selected_insect = {};
