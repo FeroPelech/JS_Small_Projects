56f47be50cda99fc72c56a7aac33504eeb6ea11c
Frantisek Pelech <feromiro23@gmail.com>
2024-04-25 00:18:05 +0200

    Added ugly date

--- Digital_Clock/script.js
+++ Digital_Clock/script.js
@@ -1,11 +1,15 @@
 let hrs = document.getElementById("hrs");
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
+let date = document.querySelector(".date");
 
 setInterval(() => {
   let currentTime = new Date();
+  let month = currentTime.getMonth() + 1;
 
   hrs.innerHTML = currentTime.getHours();
   min.innerHTML = currentTime.getMinutes();
   sec.innerHTML = currentTime.getSeconds();
+  date.innerHTML =
+    currentTime.getDate() + ":" + month + ":" + currentTime.getFullYear();
 }, 1000);
