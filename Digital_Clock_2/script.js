e49d36ff5398fc0de5841d510b6a49188297a5a8
Frantisek Pelech <feromiro23@gmail.com>
2024-04-26 22:44:31 +0200

    Created arrays of months and days and prepared setInterval function

---
+++ Digital_Clock_2/script.js
@@ -0,0 +1,29 @@
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const indexDate = document.getElementById("Date");
+
+setInterval(() => {
+  let date = new Date();
+  document.getElementById("Date");
+}, 1000);
