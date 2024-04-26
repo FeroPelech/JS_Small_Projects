540384c58065ad170bc6c084d27bf0f7f4e760dd
Frantisek Pelech <feromiro23@gmail.com>
2024-04-27 00:11:21 +0200

    All done

--- Digital_Clock_2/script.js
+++ Digital_Clock_2/script.js
@@ -22,6 +22,9 @@ const days = [
   "Saturday",
 ];
 let indexDate = document.getElementById("date");
+let hours = document.getElementById("hours");
+let minutes = document.getElementById("minutes");
+let seconds = document.getElementById("seconds");
 
 setInterval(() => {
   let date = new Date();
@@ -33,4 +36,10 @@ setInterval(() => {
     months[date.getMonth()] +
     " " +
     date.getFullYear();
+  hours.innerHTML =
+    date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+  minutes.innerHTML =
+    date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+  seconds.innerHTML =
+    date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
 }, 1000);
