6bbd51f219408dee5dc19108520659e00160ee5c
Frantisek Pelech <feromiro23@gmail.com>
2024-04-30 23:31:36 +0200

    Created formatTime function

--- Timer/script.js
+++ Timer/script.js
@@ -2,3 +2,16 @@ const restBtn = document.getElementById("reset");
 const playBtn = document.getElementById("play");
 const timerEl = document.getElementById("timer");
 const root = document.querySelector(":root");
+
+const totalseconds = 60;
+let playing = false;
+let currentSeconds = totalseconds;
+timerEl.innerText = formatTime(totalseconds);
+
+function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const newSeconds = seconds % 60;
+  return `${minutes.toString().padStart(2, "0")}:${newSeconds
+    .toString()
+    .padStart(2, "0")}`;
+}
