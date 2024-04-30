7e020d51a335222ba81b99e5e4d2c8b6b4947e2a
Frantisek Pelech <feromiro23@gmail.com>
2024-04-30 23:25:18 +0200

    Created 4 constants html element

---
+++ Timer/script.js
@@ -0,0 +1,4 @@
+const restBtn = document.getElementById("reset");
+const playBtn = document.getElementById("play");
+const timerEl = document.getElementById("timer");
+const root = document.querySelector(":root");
