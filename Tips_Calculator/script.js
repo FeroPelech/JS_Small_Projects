e2f5c4f42e075097347b6b851246df6cc3f89174
Frantisek Pelech <feromiro23@gmail.com>
2024-05-03 23:24:48 +0200

    Created message

--- Tips_Calculator/script.js
+++ Tips_Calculator/script.js
@@ -4,5 +4,5 @@ const output = document.querySelector(".output");
 btn.addEventListener("click", () => {
   const cost = document.querySelector("input");
   let tip = (cost.value * 0.15).toFixed(2);
-  console.log(tip);
+  output.innerHTML = `<h1>You should tip $${tip} on $${cost.value} </h1>`;
 });
