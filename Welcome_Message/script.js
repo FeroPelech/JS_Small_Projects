5ec9fe8c9ee007aa7c8f8d5a228e9c4e144782ae
Frantisek Pelech <feromiro23@gmail.com>
2024-05-05 19:31:51 +0200

    Listener for button which start showMessage function

--- Welcome_Message/script.js
+++ Welcome_Message/script.js
@@ -1,3 +1,5 @@
 const output = document.querySelector(".output");
 const button = document.querySelector("button");
 const myName = document.querySelector("input");
+
+button.addEventListener("click", showMessage);
