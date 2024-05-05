a9f6c1d3d2ab7c044a16dd7fa3293ac443d8dd00
Frantisek Pelech <feromiro23@gmail.com>
2024-05-05 19:33:46 +0200

    showMessage function

--- Welcome_Message/script.js
+++ Welcome_Message/script.js
@@ -3,3 +3,7 @@ const button = document.querySelector("button");
 const myName = document.querySelector("input");
 
 button.addEventListener("click", showMessage);
+
+function showMessage() {
+  output.innerHTML = `<h1>Hello welcome ` + myName.value + ` </h1>`;
+}
