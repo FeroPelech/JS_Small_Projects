fa50d31e6c78c3f030b7cd25bb8509bce8ae4613
Frantisek Pelech <feromiro23@gmail.com>
2024-05-08 10:19:52 +0200

    Created listener for button and showOutput function with simple if condition

--- Dynamically_Hello/script.js
+++ Dynamically_Hello/script.js
@@ -1,2 +1,19 @@
 const button = document.querySelector("button");
 const output = document.querySelector(".output");
+let message;
+
+button.addEventListener("click", showOutput);
+
+function showOutput() {
+  const date = new Date();
+  let current = date.getHours();
+  if (current > 17) {
+    message = "Its evening";
+  } else if (current > 12) {
+    message = "Its afternoon";
+  } else if (current > 0) {
+    message = "Its morning";
+  } else {
+    message = "Something is wrong";
+  }
+}
