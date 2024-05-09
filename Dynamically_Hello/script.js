79c4736a6990c23b3a3e32ae26bf6639428f8e9d
Frantisek Pelech <feromiro23@gmail.com>
2024-05-09 22:38:28 +0200

    Style for output class

--- Dynamically_Hello/script.js
+++ Dynamically_Hello/script.js
@@ -9,12 +9,16 @@ function showOutput() {
   let message;
   if (current > 17) {
     message = "Its evening";
+    output.style.backgroundColor = "black";
   } else if (current > 12) {
     message = "Its afternoon";
+    output.style.backgroundColor = "purple";
   } else if (current > 0) {
     message = "Its morning";
+    output.style.backgroundColor = "green";
   } else {
     message = "Something is wrong";
+    output.style.backgroundColor = "red";
   }
   output.innerHTML = `<h1>` + message + `</h1>`;
 }
