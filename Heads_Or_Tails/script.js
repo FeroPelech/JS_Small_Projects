a0caa53db7936641b27e0684ed99a1694333c882
Frantisek Pelech <feromiro23@gmail.com>
2024-05-12 15:03:16 +0200

    Created game logic

--- Heads_Or_Tails/script.js
+++ Heads_Or_Tails/script.js
@@ -13,4 +13,18 @@ function tossCoin(e) {
   let computerGuess = coins[toss];
   message.innerHTML = "Computer Selected " + computerGuess + "<br>";
   let output;
+
+  if (playerGuess === computerGuess) {
+    output = "You Wins!!!";
+    score[0]++;
+  } else {
+    output = "You Lose and computer Wins!!!";
+    score[1]++;
+  }
+  message.innerHTML +=
+    output +
+    "<br>Your score is: " +
+    score[0] +
+    "Computer score is: " +
+    score[1];
 }
