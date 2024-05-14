c200e2f4e07358860958ee44bce3ca98e8df173d
Frantisek Pelech <feromiro23@gmail.com>
2024-05-14 09:06:28 +0200

    All html constants

---
+++ Rock_Paper_Scissors/script.js
@@ -0,0 +1,5 @@
+const score = document.querySelector(".score");
+const message = document.querySelector(".message");
+const buttons = document.querySelectorAll("button");
+
+console.log(buttons[0].textContent);
