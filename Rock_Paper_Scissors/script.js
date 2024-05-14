180345233239b63c54b8e28dc61d46bba8d83040
Frantisek Pelech <feromiro23@gmail.com>
2024-05-14 09:13:51 +0200

    Added listener for buttons and created gameLogic function

--- Rock_Paper_Scissors/script.js
+++ Rock_Paper_Scissors/script.js
@@ -3,3 +3,11 @@ const message = document.querySelector(".message");
 const buttons = document.querySelectorAll("button");
 
 console.log(buttons[0].textContent);
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", gameLogic);
+}
+
+function gameLogic(e) {
+  console.log(e.target.textContent);
+}
