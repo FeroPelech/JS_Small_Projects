5b902d5d4b6947e13c5a75d1ed4b0975a016de19
Frantisek Pelech <feromiro23@gmail.com>
2024-05-20 16:24:35 +0200

    Smoll correction and creted constants for html element

---
+++ Dice_Game/script.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("btn");
+const player1 = document.getElementById("player1");
+const player2 = document.getElementById("player2");
+const output = document.getElementById("output");
+const output1 = document.querySelector(".output");
+
+console.log(output);
+console.log(output1);
