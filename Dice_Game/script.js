c28804b431a5b408da5370da973d03e01b34c246
Frantisek Pelech <feromiro23@gmail.com>
2024-05-21 15:00:23 +0200

    Created functions updateOutput and builder

--- Dice_Game/script.js
+++ Dice_Game/script.js
@@ -1,16 +1,18 @@
 const button = document.querySelector("button");
-const player1 = document.getElementById("player1");
-const player2 = document.getElementById("player2");
-const output = document.getElementById("output");
-const output1 = document.querySelector(".output");
-
-console.log(output);
-console.log(output1);
+const player1 = document.querySelector(".player1");
+const player2 = document.querySelector(".player2");
+const output = document.querySelector(".output");
+const dice = [
+  [5],
+  [1, 9],
+  [1, 5, 9],
+  [1, 3, 7, 9],
+  [1, 3, 5, 7, 9],
+  [1, 3, 4, 6, 7, 9],
+];
 
 button.addEventListener("click", () => {
-  console.log("ok");
   let rolls = [roll(6), roll(6)];
-  console.log(rolls);
   let temp;
   if (rolls[0] === rolls[1]) {
     temp = "Draw";
@@ -19,16 +21,36 @@ button.addEventListener("click", () => {
   } else {
     temp = "Player 2 wins";
   }
+
+  updateOutput(player1, rolls[0]);
+  updateOutput(player2, rolls[1]);
   output.innerHTML = temp + "	&#128;";
-  player1.innerHTML = rolls[0];
-  player2.innerHTML = rolls[1];
 });
+
+function updateOutput(el, num) {
+  let holder = builder(num);
+  if (el.children[0]) {
+    el.children[0].remove();
+  }
+  el.appendChild(holder);
+}
+
+function builder(num) {
+  let div = document.createElement("div");
+  let diceArray = dice[num - 1];
+  for (let i = 1; i < 10; i++) {
+    let span = document.createElement("div");
+    span.setAttribute("class", "dot");
+    if (diceArray.includes(i)) {
+      span.classList.add("black");
+    }
+    div.appendChild(span);
+  }
+  div.setAttribute("class", "dicer");
+  return div;
+}
 
 function roll(num) {
   let randonNumber = Math.floor(Math.random() * num) + 1;
-  let n = 9855 + randonNumber;
-  let char = "&#" + n + ";";
-  // code for dices are from code table .net
-  console.log(char);
-  return randonNumber + " " + char;
+  return randonNumber;
 }
