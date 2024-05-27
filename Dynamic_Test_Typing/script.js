32b2ddf971afd7b1339a714e67ebd32fa1a80690
Frantisek Pelech <feromiro23@gmail.com>
2024-05-27 16:04:48 +0200

    Created 4 constants in script.js for approaching to the html elements

---
+++ Dynamic_Test_Typing/script.js
@@ -0,0 +1,8 @@
+const wording = [
+  "How much do you love Arsenal FC?",
+  "Hope you are not spurs fan...",
+  "The worst teams are spurs, ugly and protected 115 city and chelsea",
+];
+const message = document.querySelector(".message");
+const playText = document.querySelector(".textarea");
+const btn = document.querySelector("button");
