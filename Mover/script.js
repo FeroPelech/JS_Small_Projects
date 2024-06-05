c01c063a7e6be8369df3038c785659b7425254c4
Frantisek Pelech <feromiro23@gmail.com>
2024-06-06 01:07:37 +0200

    Created class for myBlock html element

--- Mover/script.js
+++ Mover/script.js
@@ -2,6 +2,8 @@ let myBlock;
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Hej");
   myBlock = document.createElement("div");
+  myBlock.className = "myBlock";
   myBlock.textContent = "Hello";
+  myBlock.style.width = "100px";
   document.body.appendChild(myBlock);
 });
