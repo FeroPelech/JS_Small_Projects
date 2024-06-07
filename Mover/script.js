854317d3acc2081c75d38b16b9a63bf5b3b94a0d
Frantisek Pelech <feromiro23@gmail.com>
2024-06-07 12:21:07 +0200

    Added color style for text

--- Mover/script.js
+++ Mover/script.js
@@ -1,11 +1,11 @@
 let myBlock;
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Hej");
   myBlock = document.createElement("div");
   myBlock.className = "myBlock";
   myBlock.textContent = "Hello";
   myBlock.style.width = "100px";
   myBlock.style.height = "100px";
   myBlock.style.backgroundColor = "Blue";
+  myBlock.style.color = "White";
   document.body.appendChild(myBlock);
 });
