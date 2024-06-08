4f42ad14a68ae4b750eb10e92a29b987e4df1dbb
Frantisek Pelech <feromiro23@gmail.com>
2024-06-08 09:45:05 +0200

    Style for myBlock lineHeight

--- Mover/script.js
+++ Mover/script.js
@@ -7,5 +7,6 @@ document.addEventListener("DOMContentLoaded", () => {
   myBlock.style.height = "100px";
   myBlock.style.backgroundColor = "Blue";
   myBlock.style.color = "White";
+  myBlock.style.lineHeight = "100px";
   document.body.appendChild(myBlock);
 });
