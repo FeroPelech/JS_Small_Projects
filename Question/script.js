18946fa026a68c28fe1e89856096e8a3cf3e6a22
Frantisek Pelech <feromiro23@gmail.com>
2024-06-18 00:28:14 +0200

    Created answer list

--- Question/script.js
+++ Question/script.js
@@ -1,3 +1,4 @@
 const message = document.querySelector(".message");
 const question = document.querySelector("input");
 const button = document.querySelector("button");
+const answers = ["It will good", "Maybe", "Probably", "I dont know", "Emmm"];
