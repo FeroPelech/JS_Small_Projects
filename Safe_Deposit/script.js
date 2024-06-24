8fc156793730cb6eb3c75710ebcd06cb971d2088
Frantisek Pelech <feromiro23@gmail.com>
2024-06-24 08:27:36 +0200

    Small correction

--- Safe_Deposit/script.js
+++ Safe_Deposit/script.js
@@ -12,13 +12,17 @@ btn.addEventListener("click", () => {
 });
 
 function maker() {
-  let el = document.createElement("input");
-  el.setAttribute("type", "number");
-  el.max = 9;
-  el.min = 0;
-  el.size = 1;
-  el.style.width = "130px";
-  el.order = 0;
-  console.log(el);
-  gameArea.appendChild(el);
+  for (let x = 0; x < 4; x++) {
+    let el = document.createElement("input");
+    el.setAttribute("type", "number");
+    el.max = 9;
+    el.min = 0;
+    el.size = 1;
+    el.value = 0;
+    el.style.width = "40px";
+    el.classList.add("numb");
+    el.value = 0;
+    el.order = x;
+    gameArea.appendChild(el);
+  }
 }
