91c05b83ff4f088a6fc41111277219e7b44e30cf
Frantisek Pelech <feromiro23@gmail.com>
2024-06-29 19:45:11 +0200

    Created 2 variables

--- Password_Checker/script.js
+++ Password_Checker/script.js
@@ -1,6 +1,8 @@
 const message = document.querySelector(".message");
 const guess = document.querySelector("input");
 const button = document.querySelector("button");
+let inPlay = false;
+const myList = ["javascript", "website", "html", "document", "course"];
 button.addEventListener("click", () => {
   console.log("kukaj");
 });
