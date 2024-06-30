6554326f4b406da161f30b9b9559714e5063ff4f
Frantisek Pelech <feromiro23@gmail.com>
2024-07-01 00:11:03 +0200

    Added word to the html document

--- Password_Checker/script.js
+++ Password_Checker/script.js
@@ -2,14 +2,15 @@ const message = document.querySelector(".message");
 const guess = document.querySelector("input");
 const button = document.querySelector("button");
 let inPlay = false;
+let scramble = "";
 const myList = ["javascript", "website", "html", "document", "course"];
 button.addEventListener("click", () => {
-  console.log("kukaj");
   if (!inPlay) {
     inPlay = true;
     button.innerHTML = "Guess";
     guess.classList.toggle("hidden");
-    console.log(createWord());
+    scramble = createWord();
+    message.innerHTML = scramble;
   }
 });
 
