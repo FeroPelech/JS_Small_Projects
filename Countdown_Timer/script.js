e9381bb02075d8e6d38ceb376ed1612d54d5d7e6
Frantisek Pelech <feromiro23@gmail.com>
2024-07-05 00:39:00 +0200

    Listener for input

--- Countdown_Timer/script.js
+++ Countdown_Timer/script.js
@@ -1,2 +1,6 @@
 const endDate = document.querySelector("input[name='endDate']");
 const clock = document.querySelector(".clock");
+
+endDate.addEventListener("change", (e) => {
+  console.log(e);
+});
