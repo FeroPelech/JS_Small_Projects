686103304007c4b439ac728a66fb18346f09ff1f
Frantisek Pelech <feromiro23@gmail.com>
2024-07-05 00:41:46 +0200

    Added temp variable

--- Countdown_Timer/script.js
+++ Countdown_Timer/script.js
@@ -2,5 +2,8 @@ const endDate = document.querySelector("input[name='endDate']");
 const clock = document.querySelector(".clock");
 
 endDate.addEventListener("change", (e) => {
-  console.log(e);
+  e.preventDefault();
+  console.log(endDate.value);
+  const temp = new Date(endDate.value);
+  console.log(temp);
 });
