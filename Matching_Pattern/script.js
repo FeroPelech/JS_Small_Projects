1b089d8701922fc9f41236efcfefff8961c90e06
Frantisek Pelech <feromiro23@gmail.com>
2024-07-13 14:15:00 +0200

    List of colors

--- Matching_Pattern/script.js
+++ Matching_Pattern/script.js
@@ -1,7 +1,10 @@
 const message = document.querySelector(".message");
 const gamearea = document.querySelector(".gamearea");
 const button = document.querySelector("button");
+const colors = ["red", "blue", "green", "yellow"];
+
 button.addEventListener("click", () => {
-  console.log(message);
-  console.log(gamearea);
+  for (let i = 0; i < colors.length; i++) {
+    console.log(colors[i]);
+  }
 });
