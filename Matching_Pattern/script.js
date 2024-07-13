2eb8a490c555e8b1e51d6bcce7b212eed38ff96d
Frantisek Pelech <feromiro23@gmail.com>
2024-07-13 14:10:56 +0200

    Listener for button

--- Matching_Pattern/script.js
+++ Matching_Pattern/script.js
@@ -1,3 +1,7 @@
 const message = document.querySelector(".message");
 const gamearea = document.querySelector(".gamearea");
 const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  console.log(message);
+  console.log(gamearea);
+});
