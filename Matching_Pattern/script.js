12fd56871ac42c3c39563d65c9363930535a8075
Frantisek Pelech <feromiro23@gmail.com>
2024-07-14 14:02:02 +0200

    Logs deleted

--- Matching_Pattern/script.js
+++ Matching_Pattern/script.js
@@ -3,26 +3,19 @@ const gamearea = document.querySelector(".gamearea");
 const button = document.querySelector("button");
 const colors = ["red", "blue", "green", "yellow"];
 
-button.addEventListener("click", () => {
-  for (let i = 0; i < colors.length; i++) {
-    console.log(colors[i]);
-  }
-});
+button.addEventListener("click", () => {});
 
 window.addEventListener("load", setup);
 
 function setup() {
-  console.log("Setup function");
   for (let i = 0; i < colors.length; i++) {
     let div = eleFactor("div");
     div.style.backgroundColor = colors[i];
     gamearea.appendChild(div);
-    console.log(div);
   }
 }
 
 function eleFactor(eleType) {
   let ele = document.createElement(eleType);
-  console.log("kuk");
   return ele;
 }
