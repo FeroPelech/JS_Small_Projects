fece8c5cf3e8bf8cf71d6c7fc9c38e6a5b3ed690
Frantisek Pelech <feromiro23@gmail.com>
2024-07-20 13:17:45 +0200

    Functionality for updateText function

--- Auto_Text_Effect/script.js
+++ Auto_Text_Effect/script.js
@@ -1,7 +1,15 @@
 const containerElemenet = document.querySelector(".container");
 const careers = ["Influencer", "Youtube", "Electrician", "Freelancer"];
 let index = 0;
+let characterIndex = 0;
+
+updateText();
 
 function updateText() {
-  containerElemenet.innerHTML = "";
+  containerElemenet.innerHTML = `I am a ${careers[index].slice(
+    0,
+    characterIndex
+  )}`;
+  characterIndex++;
+  setTimeout(updateText, 400);
 }
