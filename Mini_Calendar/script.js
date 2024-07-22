0ecfdac497cc469a5e9fba33648b56aa27298674
Frantisek Pelech <feromiro23@gmail.com>
2024-07-22 17:44:31 +0200

    Added day to html

--- Mini_Calendar/script.js
+++ Mini_Calendar/script.js
@@ -2,11 +2,14 @@ const monthN = document.getElementById("month-name");
 const dayN = document.getElementById("day-name");
 const dayNum = document.getElementById("day-number");
 const year = document.getElementById("year");
+const days = [""];
 
 const date = new Date();
 const month = date.getMonth();
 const cMonth = date.toLocaleString("sk", { month: "long" });
-const aYear = date.getFullYear();
-console.log(aYear);
+const cDay = date.toLocaleString("sk", { weekday: "long" });
+const cYear = date.getFullYear();
+
 monthN.innerText = cMonth.charAt(0).toUpperCase() + cMonth.slice(1);
-year.innerText = aYear;
+year.innerText = cYear;
+dayN.innerText = cDay.charAt(0).toUpperCase() + cDay.slice(1);
