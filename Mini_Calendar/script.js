b07704f989267b0c606bad52e157597e44194cbe
Frantisek Pelech <feromiro23@gmail.com>
2024-07-22 15:03:41 +0200

    All constants date html elements

---
+++ Mini_Calendar/script.js
@@ -0,0 +1,4 @@
+const monthN = document.getElementById("month-name");
+const dayN = document.getElementById("day-name");
+const dayNum = document.getElementById("day-number");
+const year = document.getElementById("year");
