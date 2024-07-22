8b38c8dbfde3904e8dbc78044cec0be751185566
Frantisek Pelech <feromiro23@gmail.com>
2024-07-22 17:40:38 +0200

    Added year to html

--- Mini_Calendar/script.js
+++ Mini_Calendar/script.js
@@ -6,5 +6,7 @@ const year = document.getElementById("year");
 const date = new Date();
 const month = date.getMonth();
 const cMonth = date.toLocaleString("sk", { month: "long" });
-
+const aYear = date.getFullYear();
+console.log(aYear);
 monthN.innerText = cMonth.charAt(0).toUpperCase() + cMonth.slice(1);
+year.innerText = aYear;
