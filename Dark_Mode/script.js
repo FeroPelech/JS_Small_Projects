3c1b793b4b1bee4ff367161062047035db92084a
Frantisek Pelech <feromiro23@gmail.com>
2024-07-29 15:33:45 +0200

    Black background

--- Dark_Mode/script.js
+++ Dark_Mode/script.js
@@ -2,7 +2,12 @@ const inputEl = document.querySelector(".input");
 const bodyEl = document.querySelector("body");
 console.log(inputEl.checked);
 
+updateBody();
+
 function updateBody() {
   if (inputEl.checked) {
+    bodyEl.style.backgroundColor = "black";
+  } else {
+    bodyEl.style.backgroundColor = "white";
   }
 }
