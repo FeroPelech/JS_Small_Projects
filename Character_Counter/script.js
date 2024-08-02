ffff90638c4290d4f15656d2f314064cee323116
Frantisek Pelech <feromiro23@gmail.com>
2024-08-02 16:48:09 +0200

    Constant for remaining-counter

--- Character_Counter/script.js
+++ Character_Counter/script.js
@@ -2,3 +2,5 @@ const textAreaEl = document.getElementById("textarea");
 console.log(textAreaEl);
 const totalCounter = document.getElementById("total-counter");
 console.log(totalCounter);
+const remainingCounter = document.getElementById("remaining-counter");
+console.log(remainingCounter);
