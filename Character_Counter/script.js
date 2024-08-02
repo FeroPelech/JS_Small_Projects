3d29938b2eb4025500e7579ea6d66250105a52a6
Frantisek Pelech <feromiro23@gmail.com>
2024-08-02 16:49:20 +0200

    Listener for textarea

--- Character_Counter/script.js
+++ Character_Counter/script.js
@@ -4,3 +4,7 @@ const totalCounter = document.getElementById("total-counter");
 console.log(totalCounter);
 const remainingCounter = document.getElementById("remaining-counter");
 console.log(remainingCounter);
+
+textAreaEl.addEventListener("keyup", () => {
+  updateCounter();
+});
