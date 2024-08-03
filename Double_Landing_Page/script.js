0f31ca0b5994fd4b31bb92c3076f5550e184ae8c
Frantisek Pelech <feromiro23@gmail.com>
2024-08-03 13:48:27 +0200

    Listener for left side(apple)

--- Double_Landing_Page/script.js
+++ Double_Landing_Page/script.js
@@ -1,3 +1,7 @@
 const container = document.querySelector(".container");
 const left = document.querySelector(".left");
 const right = document.querySelector(".right");
+
+left.addEventListener("mouseenter", () => {
+  console.log("kuk");
+});
