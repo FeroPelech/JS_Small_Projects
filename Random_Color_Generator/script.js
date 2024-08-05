2d4b267d04826d6e0ed1f899d935f3833c9945fe
Frantisek Pelech <feromiro23@gmail.com>
2024-08-05 14:13:59 +0200

    Loop for color containers

--- Random_Color_Generator/script.js
+++ Random_Color_Generator/script.js
@@ -1,1 +1,7 @@
 const container = document.querySelector(".color-containers");
+
+for (let i = 0; i < 30; i++) {
+  const colorContainerEl = document.createElement("div");
+  colorContainerEl.classList.add("color-container");
+  container.appendChild(colorContainerEl);
+}
