444edad0277b13e8be6095fcc3b14c97ab0c93ee
Frantisek Pelech <feromiro23@gmail.com>
2024-08-05 14:29:37 +0200

    Random color code generator

--- Random_Color_Generator/script.js
+++ Random_Color_Generator/script.js
@@ -4,6 +4,16 @@ for (let i = 0; i < 30; i++) {
   const colorContainerEl = document.createElement("div");
   colorContainerEl.classList.add("color-container");
   container.appendChild(colorContainerEl);
+  randomColor();
 }
 
-function randomColor() {}
+function randomColor() {
+  const chars = "0123456789abcdef";
+  const colorCodeLength = 6;
+  let color = "";
+  for (let i = 0; i < colorCodeLength; i++) {
+    const randomNumber = Math.floor(Math.random() * chars.length);
+    color += chars.substring(randomNumber, randomNumber + 1);
+    console.log(color);
+  }
+}
