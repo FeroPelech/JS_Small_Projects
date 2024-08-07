40cd7b45db13950c53766ac3062a3cd9357c0990
Frantisek Pelech <feromiro23@gmail.com>
2024-08-07 14:03:23 +0200

    Added variables and constants

---
+++ Rotation_Image_Gallery/script.js
@@ -0,0 +1,6 @@
+const imageContainer = document.querySelector(".image-container");
+
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+
+let x = 0;
