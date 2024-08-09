ef7730ef145a1950c786bb9189aaeb6c394cb0f6
Frantisek Pelech <feromiro23@gmail.com>
2024-08-10 01:07:01 +0200

    Problem solved

--- Happy_New_Year_Countdown/script.js
+++ Happy_New_Year_Countdown/script.js
@@ -3,13 +3,13 @@ const hourEl = document.getElementById("hour");
 const minuteEl = document.getElementById("minute");
 const secondEl = document.getElementById("second");
 
-const newYearDate = new Date("Jan 1, 2025 00:00:00").getTime();
+const newYearDate = new Date("Jan 1, 2024 00:00:00").getTime();
 
 updateCountDown();
 
 function updateCountDown() {
   const actualTime = new Date().getTime();
-  console.log(actualTime);
+  console.log(actualTime + "   " + newYearDate);
   const different = newYearDate - actualTime;
 
   const second = 1000;
@@ -17,13 +17,14 @@ function updateCountDown() {
   const hour = minute * 60;
   const day = hour * 24;
 
-  const d = Math.floor(actualTime / day);
-  const h = Math.floor((actualTime % day) / hour);
-  const m = Math.floor((actualTime % hour) / minute);
-  const s = Math.floor((actualTime % minute) / second);
+  const d = 366 + Math.floor(different / day);
+  const h = 23 + Math.floor((different % day) / hour);
+  const m = 60 + Math.floor((different % hour) / minute);
+  const s = 60 + Math.floor((different % minute) / second);
 
   dayEl.innerText = d;
   hourEl.innerText = h;
   minuteEl.innerText = m;
   secondEl.innerText = s;
+  setTimeout(updateCountDown, 1000);
 }
