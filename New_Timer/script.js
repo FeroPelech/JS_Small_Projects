86593af772755cab45e09e3d3c15d550cec1aba8
Frantisek Pelech <feromiro23@gmail.com>
2024-08-11 18:28:53 +0200

    Done

--- New_Timer/script.js
+++ New_Timer/script.js
@@ -6,17 +6,20 @@ const submit = document.getElementById("submit");
 const input = document.getElementById("input");
 let defaultTime = 0;
 let userTime;
-let run = false;
+let valueForReset;
 let interval;
 
 function updateTimer() {
   let minutes = Math.floor(userTime / 60);
   let seconds = userTime % 60;
-  let formattedTime = `${minutes.toString().padStart(2, 0)}:${seconds}`;
+  let formattedTime = `${minutes.toString().padStart(2, 0)}:${seconds
+    .toString()
+    .padStart(2, 0)}`;
   timer.innerHTML = formattedTime;
 }
 
 submit.addEventListener("click", () => {
+  valueForReset = input.value * 60;
   if (input.value < 1) {
     alert("You cannot insert negative time");
   } else if (input.value < 10 && input.value > 0) {
@@ -30,7 +33,6 @@ submit.addEventListener("click", () => {
 });
 
 start.addEventListener("click", () => {
-  console.log(userTime);
   startTimer();
 });
 
@@ -43,17 +45,22 @@ reset.addEventListener("click", () => {
 });
 
 function startTimer() {
-  setInterval(() => {
+  interval = setInterval(() => {
     userTime--;
-    console.log(userTime);
     updateTimer();
+    if (userTime === 0) {
+      clearInterval(interval);
+    }
   }, 1000);
 }
 
 function stopTimer() {
   console.log("stop");
+  clearInterval(interval);
 }
 
 function resetTimer() {
   console.log("reset");
+  userTime = valueForReset;
+  updateTimer();
 }
