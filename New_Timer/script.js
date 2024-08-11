14e942fc0dfeee1eb9fae54bea98e42bad0b029c
Frantisek Pelech <feromiro23@gmail.com>
2024-08-11 17:52:28 +0200

    Timer countdown work

--- New_Timer/script.js
+++ New_Timer/script.js
@@ -5,14 +5,50 @@ const timer = document.getElementById("timer");
 const submit = document.getElementById("submit");
 const input = document.getElementById("input");
 let defaultTime = 0;
+let userTime;
+let run = false;
+let interval;
+
+function updateTimer() {
+  let minutes = Math.floor(userTime / 60);
+  let seconds = userTime % 60;
+  let formattedTime = minutes + ":" + seconds;
+  timer.innerHTML = formattedTime;
+}
 
 submit.addEventListener("click", () => {
-  defaultTime = input.value;
-  if (defaultTime > 9) {
-    timer.innerText = defaultTime + ":00";
-  } else if (defaultTime < 9 && defaultTime > 0) {
-    timer.innerText = "0" + defaultTime + ":00";
-  } else {
+  if (input.value < 1) {
     alert("You cannot insert negative time");
+  } else {
+    userTime = input.value * 60;
   }
 });
+
+start.addEventListener("click", () => {
+  console.log(userTime);
+  startTimer();
+});
+
+stopE.addEventListener("click", () => {
+  stopTimer();
+});
+
+reset.addEventListener("click", () => {
+  resetTimer();
+});
+
+function startTimer() {
+  setInterval(() => {
+    userTime--;
+    console.log(userTime);
+    updateTimer();
+  }, 1000);
+}
+
+function stopTimer() {
+  console.log("stop");
+}
+
+function resetTimer() {
+  console.log("reset");
+}
