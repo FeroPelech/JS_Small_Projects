ac2392973d374e96cfa639113c1b3adeb2182e13
Frantisek Pelech <feromiro23@gmail.com>
2024-08-12 14:49:32 +0200

    2 constants, listener for button which call calculateAge function

---
+++ Age_Calculator/script.js
@@ -0,0 +1,9 @@
+const btnEl = document.getElementById("btn");
+const birthdayEl = document.getElementById("birthday");
+console.log(birthdayEl);
+
+function calculateAge() {
+  console.log("Kuk");
+}
+
+btnEl.addEventListener("click", calculateAge);
