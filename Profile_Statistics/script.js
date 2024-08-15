ff866c39627ac8bdd8699c0f9657085de780ac5c
Frantisek Pelech <feromiro23@gmail.com>
2024-08-15 17:27:54 +0200

    Small correction 2

--- Profile_Statistics/script.js
+++ Profile_Statistics/script.js
@@ -2,14 +2,17 @@ const counters = document.querySelectorAll(".counter");
 
 counters.forEach((e) => {
   e.innerText = "0";
-  incrementCounter(e);
+  incrementCounter();
+  function incrementCounter() {
+    let currentNum = +e.innerText;
+    const dataCeil = e.getAttribute("data-ceil");
+    const increment = dataCeil / 15;
+    currentNum = Math.floor(currentNum + increment);
+    e.innerText = currentNum;
+    if (currentNum < dataCeil) {
+      e.innerText = currentNum;
+      setTimeout(incrementCounter, 50);
+    }
+    console.log(dataCeil);
+  }
 });
-
-function incrementCounter(e) {
-  let currentNum = +e.innerText;
-  const dataCeil = e.getAttribute("data-ceil");
-  const increment = dataCeil / 15;
-  currentNum = Math.ceil(currentNum + increment);
-  e.innerText = currentNum;
-  console.log(dataCeil);
-}
