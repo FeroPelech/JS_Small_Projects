6ea06dffc26c72d0082bf6bfc06fe5f9387f92d6
Frantisek Pelech <feromiro23@gmail.com>
2024-08-15 17:31:57 +0200

    Done

--- Profile_Statistics/script.js
+++ Profile_Statistics/script.js
@@ -7,12 +7,13 @@ counters.forEach((e) => {
     let currentNum = +e.innerText;
     const dataCeil = e.getAttribute("data-ceil");
     const increment = dataCeil / 15;
-    currentNum = Math.floor(currentNum + increment);
+    currentNum = Math.ceil(currentNum + increment);
     e.innerText = currentNum;
     if (currentNum < dataCeil) {
       e.innerText = currentNum;
       setTimeout(incrementCounter, 50);
+    } else {
+      e.innerText = dataCeil;
     }
-    console.log(dataCeil);
   }
 });
