4deb8f78530348342cb754645804537d4c68cf64
Frantisek Pelech <feromiro23@gmail.com>
2024-08-17 13:03:54 +0200

    Listener for button

--- Video_Popup/script.js
+++ Video_Popup/script.js
@@ -1,3 +1,5 @@
 const btn = document.querySelector(".btn");
 const closeIcon = document.querySelector(".close-icon");
 const trailerContainer = document.querySelector(".trailer-container");
+
+btn.addEventListener("click", () => {});
