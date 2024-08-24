5a22d2f28f1b43c5f0effd286f172c359c2c6550
Frantisek Pelech <feromiro23@gmail.com>
2024-08-24 11:06:18 +0200

    Added 3 constants for temperature units

--- Temperature_Convertor/script.js
+++ Temperature_Convertor/script.js
@@ -1,3 +1,7 @@
+const celsius = document.getElementById("celsius");
+const fahrenheit = document.getElementById("fahrenheit");
+const kelvin = document.getElementById("kelvin");
+
 function computeTemp(e) {
   const currentValue = e.target.value;
   console.log(currentValue);
