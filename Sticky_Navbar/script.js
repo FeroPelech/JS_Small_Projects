9521d4b0f560fe46790c88ad790cc25a11991398
Frantisek Pelech <feromiro23@gmail.com>
2024-08-27 23:41:53 +0200

    Added another logo image

--- Sticky_Navbar/script.js
+++ Sticky_Navbar/script.js
@@ -1,11 +1,18 @@
 const navbar = document.querySelector(".navbar");
 const bottomContainer = document.querySelector(".bottom-container");
+const image = document.querySelector("img");
+
+console.log(image.src);
 console.log(bottomContainer.offsetTop);
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > bottomContainer.offsetTop - navbar.offsetHeight - 50) {
     navbar.classList.add("active");
+    image.src =
+      "http://127.0.0.1:5500/JS_Small_Projects/Sticky_Navbar/Logo/BlackLogo.png";
   } else {
     navbar.classList.remove("active");
+    image.src =
+      "http://127.0.0.1:5500/JS_Small_Projects/Sticky_Navbar/Logo/logo.png";
   }
 });
