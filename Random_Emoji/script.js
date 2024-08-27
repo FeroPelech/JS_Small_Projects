3813892d44594e0a34006c3280fa4747e4a2af09
Frantisek Pelech <feromiro23@gmail.com>
2024-08-28 00:48:24 +0200

    Emoji array

--- Random_Emoji/script.js
+++ Random_Emoji/script.js
@@ -2,6 +2,8 @@ const btn = document.querySelector("button");
 const para = document.querySelector("p");
 console.log(para);
 
+const Emoji = [];
+
 btn.addEventListener("click", () => {
   console.log("Clicked");
 });
