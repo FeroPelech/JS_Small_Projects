62e87ae3fef65024113414cbc2a16266fd9015b3
Frantisek Pelech <feromiro23@gmail.com>
2024-08-28 00:52:00 +0200

    getEmoji function

--- Random_Emoji/script.js
+++ Random_Emoji/script.js
@@ -4,6 +4,16 @@ console.log(para);
 
 const Emoji = [];
 
+async function getEmoji() {
+  let response = await fetch(
+    "http://emoji-api.com/emojis?access_key=6d34f1e3d8da09a09dd1598e030f5603e89ac084"
+  );
+  data = await response.json();
+  console.log(data);
+}
+
+getEmoji();
+
 btn.addEventListener("click", () => {
   console.log("Clicked");
 });
