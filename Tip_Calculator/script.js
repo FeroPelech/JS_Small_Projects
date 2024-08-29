b79e73011625b8bc5e370c1aa0d92619fd967516
Frantisek Pelech <feromiro23@gmail.com>
2024-08-29 15:37:26 +0200

    4 constants for html elements

---
+++ Tip_Calculator/script.js
@@ -0,0 +1,4 @@
+const btn = document.getElementById("claculate");
+const tip = document.getElementById("tip");
+const bill = document.getElementById("bill");
+const result = document.getElementById("total");
