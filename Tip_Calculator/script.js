55814263d59b64520bb49551e9c73c25103a0115
Frantisek Pelech <feromiro23@gmail.com>
2024-08-29 15:39:23 +0200

    Listener for button which activate fucntion calculation

--- Tip_Calculator/script.js
+++ Tip_Calculator/script.js
@@ -1,4 +1,10 @@
-const btn = document.getElementById("claculate");
+const btn = document.getElementById("calculate");
 const tip = document.getElementById("tip");
 const bill = document.getElementById("bill");
 const result = document.getElementById("total");
+
+btn.addEventListener("click", calculation);
+
+function calculation() {
+  console.log("Kukaj");
+}
