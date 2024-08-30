c6684bc7b0c5cdd908ba01452f2cbe446605f247
Frantisek Pelech <feromiro23@gmail.com>
2024-08-31 01:03:22 +0200

    Function updateNum done

--- Loading_Bar/script.js
+++ Loading_Bar/script.js
@@ -4,4 +4,11 @@ const barF = document.querySelector(".loading-bar-front");
 let idx = 0;
 
 updateNum();
-function updateNum() {}
+function updateNum() {
+  counter.innerText = idx + "%";
+  barF.computedStyleMap.width = idx + "%";
+  idx++;
+  if (idx < 101) {
+    setTimeout(updateNum, 20);
+  }
+}
