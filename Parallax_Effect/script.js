a64b09df642baedb68987ba2fa413eeca9862cee
Frantisek Pelech <feromiro23@gmail.com>
2024-09-01 14:34:52 +0200

    Problem solved

--- Parallax_Effect/script.js
+++ Parallax_Effect/script.js
@@ -4,10 +4,10 @@ const card = document.querySelector(".card");
 container.addEventListener("mousemove", (e) => {
   const rect = card.getBoundingClientRect();
   const x = e.clientX - rect.left - rect.width / 2;
-  const y = e.clientY - rect.top - rect.width / 2;
+  const y = e.clientY - rect.top - rect.height / 2;
 
-  const rotateX = (x / rect.height) * 40;
-  const rotateY = (y / rect.width) * 40;
+  const rotateX = (y / rect.height) * 40;
+  const rotateY = (x / rect.width) * 40;
 
   card.style.transform = `rotateX(${-rotateX}deg) rotateY(${rotateY}deg)`;
 });
