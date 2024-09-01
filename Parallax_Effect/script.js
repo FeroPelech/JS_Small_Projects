dce19e581f9e4bc7d4ef3b0daaba43151170a3e2
Frantisek Pelech <feromiro23@gmail.com>
2024-09-01 14:23:51 +0200

    Constants for container and card with listenr for container

---
+++ Parallax_Effect/script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector(".container");
+const card = document.querySelector(".card");
+
+container.addEventListener("mousemove", (e) => {
+  const rect = card.getBoundingClientRect();
+});
