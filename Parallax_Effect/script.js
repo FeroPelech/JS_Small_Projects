48f7bafa3fbd7ea9e97529592c67fba694d7b5d3
Frantisek Pelech <feromiro23@gmail.com>
2024-09-01 14:30:29 +0200

    JS code done

--- Parallax_Effect/script.js
+++ Parallax_Effect/script.js
@@ -3,4 +3,15 @@ const card = document.querySelector(".card");
 
 container.addEventListener("mousemove", (e) => {
   const rect = card.getBoundingClientRect();
+  const x = e.clientX - rect.left - rect.width / 2;
+  const y = e.clientY - rect.top - rect.width / 2;
+
+  const rotateX = (x / rect.height) * 40;
+  const rotateY = (y / rect.width) * 40;
+
+  card.style.transform = `rotateX(${-rotateX}deg) rotateY(${rotateY}deg)`;
+});
+
+container.addEventListener("mouseleave", () => {
+  card.style.transform = `rotateX(0) rotateY(0)`;
 });
