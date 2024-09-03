2fd8efeaf04733a1df19dd19a22ac8ae87de71ef
Frantisek Pelech <feromiro23@gmail.com>
2024-09-03 20:14:55 +0200

    Done

--- Mouse_Position/script.js
+++ Mouse_Position/script.js
@@ -11,3 +11,16 @@ window.addEventListener("mousemove", (e) => {
             <h4>Mouse Y Position(px)</h4>
         </div>`;
 });
+
+window.addEventListener("mouseout", (e) => {
+  console.log(e);
+  container.innerHTML = `
+    <div class="mouse_position">
+      Nan
+      <h4>Mouse X Position(px)</h4>
+  </div>
+  <div class="mouse_position">
+      Nan
+      <h4>Mouse Y Position(px)</h4>
+  </div>`;
+});
