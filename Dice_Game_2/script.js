f3f20e4213eff60678d14ca83220d9a9e47786a9
Frantisek Pelech <feromiro23@gmail.com>
2024-09-05 21:10:16 +0200

    Added and removed roll-animation class

--- Dice_Game_2/script.js
+++ Dice_Game_2/script.js
@@ -3,4 +3,8 @@ const dice = document.getElementById("dice");
 
 btn.addEventListener("click", () => {
   console.log("Clicked");
+  dice.classList.add("roll-animation");
+  setTimeout(() => {
+    dice.classList.remove("roll-animation");
+  }, 1000);
 });
