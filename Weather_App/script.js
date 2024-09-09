211844c347fb81713c874ae558223c0f9a03cf14
Frantisek Pelech <feromiro23@gmail.com>
2024-09-09 21:47:49 +0200

    Listener for form

--- Weather_App/script.js
+++ Weather_App/script.js
@@ -2,4 +2,7 @@ const apikey = "5f83b22342ac568fae7ab18e1191885c";
 const weatherData = document.getElementById("weather-data");
 const cityInput = document.getElementById("city-input");
 const form = document.querySelector("form");
-console.log(form);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
