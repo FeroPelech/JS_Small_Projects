b81dbfd4c4c4ea861624cc29f2fc46411f1cccf5
Frantisek Pelech <feromiro23@gmail.com>
2024-09-17 18:28:36 +0200

    Added constant for search-results

--- Image_Search_App/script.js
+++ Image_Search_App/script.js
@@ -1,3 +1,4 @@
 const accessKey = "RZEIOVfPhS7vMLkFdd2TSKGFBS4o9_FmcV1Nje3FSjw";
 const formEl = document.querySelector("form");
 const searchInputEl = document.getElementById("search-input");
+const searchResultsEl = document.getElementById(".search-results");
