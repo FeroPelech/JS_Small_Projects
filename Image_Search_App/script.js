c4c8fd2b17923cd6c6b30d5aee677b06c0da40b1
Frantisek Pelech <feromiro23@gmail.com>
2024-09-17 18:31:20 +0200

    Added variable for input data

--- Image_Search_App/script.js
+++ Image_Search_App/script.js
@@ -3,3 +3,5 @@ const formEl = document.querySelector("form");
 const searchInputEl = document.getElementById("search-input");
 const searchResultsEl = document.getElementById(".search-results");
 const showMoreBtn = document.getElementById("show-more-button");
+
+let inputData = "";
