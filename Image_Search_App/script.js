381d3beffe15613e776642b0ddcf4477f6ca9f86
Frantisek Pelech <feromiro23@gmail.com>
2024-09-17 18:29:00 +0200

    Added constant for show more button

--- Image_Search_App/script.js
+++ Image_Search_App/script.js
@@ -2,3 +2,4 @@ const accessKey = "RZEIOVfPhS7vMLkFdd2TSKGFBS4o9_FmcV1Nje3FSjw";
 const formEl = document.querySelector("form");
 const searchInputEl = document.getElementById("search-input");
 const searchResultsEl = document.getElementById(".search-results");
+const showMoreBtn = document.getElementById("show-more-button");
