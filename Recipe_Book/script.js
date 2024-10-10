551e40681b0f4a9356eeb54935ac93f13f535d15
Frantisek Pelech <feromiro23@gmail.com>
2024-10-10 19:23:23 +0200

    Fetch data

--- Recipe_Book/script.js
+++ Recipe_Book/script.js
@@ -1,9 +1,16 @@
-const ApiKey = "ecf904399dc048329b5c8e64918475e6";
+const ApiKey = "af9f58a2b7444c4ebb81749f55532fc4";
 
-async function getRecipes() {}
+async function getRecipes() {
+  const response = await fetch(
+    `https://api.spoonacular.com/recipes/random?number=10&apiKey=${ApiKey}`
+  );
+  const data = await response.json();
+  return data;
+}
 
 async function init() {
   const recipes = await getRecipes();
+  console.log(recipes);
 }
 
 init();
