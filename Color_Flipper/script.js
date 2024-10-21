60d1030035d901b02cdda43955ed29ece9b5ef98
Frantisek Pelech <feromiro23@gmail.com>
2024-10-21 20:00:57 +0200

    Constant for html class color

--- Color_Flipper/script.js
+++ Color_Flipper/script.js
@@ -1,4 +1,5 @@
-const color = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 console.log(color);
 const btn = document.getElementById("btn");
 console.log(btn);
+const color = document.querySelector(".color");
