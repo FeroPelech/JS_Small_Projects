38bef0263dfb8c04ba4aada846dc74c48004811a
Frantisek Pelech <feromiro23@gmail.com>
2024-10-22 19:25:54 +0200

    Listener on button class btn

--- Color_Flipper/script.js
+++ Color_Flipper/script.js
@@ -1,5 +1,8 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-console.log(color);
+console.log(colors);
 const btn = document.getElementById("btn");
 console.log(btn);
 const color = document.querySelector(".color");
+btn.addEventListener("click", () => {
+  console.log("btnListener");
+});
