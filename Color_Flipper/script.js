92d357308f5541337af53c46c184d668c3b19a8d
Frantisek Pelech <feromiro23@gmail.com>
2024-10-28 19:38:37 +0100

    Small correction

--- Color_Flipper/script.js
+++ Color_Flipper/script.js
@@ -5,7 +5,7 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 btn.addEventListener("click", () => {
   const randomNumber = getRandomNumber();
   console.log(randomNumber);
-  color.textContent = "#" + colors[randomNumber];
+  color.textContent = "#" + randomNumber;
   document.body.style.backgroundColor = "#" + randomNumber;
 });
 
