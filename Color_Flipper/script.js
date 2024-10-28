6b0094a7d341e156ec446e4143e86a7985ffb42b
Frantisek Pelech <feromiro23@gmail.com>
2024-10-28 19:35:50 +0100

    Created hex code for background color

--- Color_Flipper/script.js
+++ Color_Flipper/script.js
@@ -3,13 +3,19 @@ console.log(colors);
 const btn = document.getElementById("btn");
 console.log(btn);
 const color = document.querySelector(".color");
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 btn.addEventListener("click", () => {
   const randomNumber = getRandomNumber();
   console.log(randomNumber);
-  document.body.style.backgroundColor = colors[randomNumber];
-  color.textContent = colors[randomNumber];
+  color.textContent = "#" + colors[randomNumber];
+  document.body.style.backgroundColor = "#" + randomNumber;
 });
 
 function getRandomNumber() {
-  return Math.floor(Math.random() * colors.length);
+  let = "";
+  for (let i = 0; i < 6; i++) {
+    let = let + hex[Math.floor(Math.random() * hex.length)];
+  }
+  console.log(let);
+  return let;
 }
