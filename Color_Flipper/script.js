21de6c35cb926f2ee6fd5800cfe7cb06ad6e7bfe
Frantisek Pelech <feromiro23@gmail.com>
2024-10-28 19:36:51 +0100

    Deleted colors array and console log

--- Color_Flipper/script.js
+++ Color_Flipper/script.js
@@ -1,5 +1,3 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-console.log(colors);
 const btn = document.getElementById("btn");
 console.log(btn);
 const color = document.querySelector(".color");
