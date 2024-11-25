4194369537d3c49ba4eddf5112ac624417e91ac9
Frantisek Pelech <feromiro23@gmail.com>
2024-11-25 20:33:25 +0100

    Variable result set on 0

--- Whac-a-mole/script.js
+++ Whac-a-mole/script.js
@@ -2,4 +2,5 @@ const squares = document.querySelectorAll(".square");
 const timeLeft = document.querySelector("#time-left");
 const mole = document.querySelector(".mole");
 const score = document.getElementById("score");
+let result = 0;
 console.log(score);
