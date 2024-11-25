4073577a7fb8dbb7623dde0e1fc0589ce48d4cd8
Frantisek Pelech <feromiro23@gmail.com>
2024-11-25 20:31:42 +0100

    Constant for score id

--- Whac-a-mole/script.js
+++ Whac-a-mole/script.js
@@ -1,4 +1,5 @@
 const squares = document.querySelectorAll(".square");
 const timeLeft = document.querySelector("#time-left");
 const mole = document.querySelector(".mole");
-console.log(mole);
+const score = document.getElementById("score");
+console.log(score);
