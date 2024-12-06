2c06a1820b26b59282b9c28bed90b09605d2f167
Frantisek Pelech <feromiro23@gmail.com>
2024-12-06 21:50:51 +0100

    Added block style bottom

--- Breakout/script.js
+++ Breakout/script.js
@@ -2,4 +2,5 @@ const grid = document.querySelector(".grid");
 const block = document.createElement("div");
 block.classList.add("block");
 block.style.left = "100px";
+block.style.bottom = "50px";
 console.log(block);
