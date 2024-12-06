a79eed8bd983b190ed1ecb9312c84791047afa77
Frantisek Pelech <feromiro23@gmail.com>
2024-12-06 21:53:39 +0100

    Created function for add new block to the html document

--- Breakout/script.js
+++ Breakout/script.js
@@ -1,7 +1,11 @@
 const grid = document.querySelector(".grid");
-const block = document.createElement("div");
-block.classList.add("block");
-block.style.left = "100px";
-block.style.bottom = "50px";
-grid.appendChild(block);
-console.log(block);
+function addBlock() {
+  const block = document.createElement("div");
+  block.classList.add("block");
+  block.style.left = "100px";
+  block.style.bottom = "50px";
+  grid.appendChild(block);
+  console.log(block);
+}
+
+addBlock();
