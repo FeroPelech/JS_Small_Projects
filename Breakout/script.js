610a7d4f39a39178fbaa67162958215d47014ba2
Frantisek Pelech <feromiro23@gmail.com>
2024-12-08 06:23:08 +0100

    Block style left set on blocks[i]bottomLeft[0]

--- Breakout/script.js
+++ Breakout/script.js
@@ -17,7 +17,7 @@ function addBlock() {
   for (let i = 0; i < blocks.length; i++) {
     const block = document.createElement("div");
     block.classList.add("block");
-    block.style.left = "100px";
+    block.style.left = blocks[i].bottomLeft[0];
     block.style.bottom = "50px";
     grid.appendChild(block);
     console.log(block);
