47be39f261905120d609582fe2542884a9cfed18
Frantisek Pelech <feromiro23@gmail.com>
2024-12-13 17:52:05 +0100

    Created variable currentPosition

--- Breakout/script.js
+++ Breakout/script.js
@@ -2,6 +2,7 @@ const grid = document.querySelector(".grid");
 const blockWidth = 100;
 const blockHeight = 20;
 const userStart = [230, 10];
+let currentPosition = userStart;
 
 class Block {
   constructor(xAxis, yAxis) {
