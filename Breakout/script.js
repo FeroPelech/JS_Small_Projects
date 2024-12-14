76fef4ff3bbcabbea0d59f4b5e06b1e5c87b713a
Frantisek Pelech <feromiro23@gmail.com>
2024-12-14 10:52:29 +0100

    Append child user

--- Breakout/script.js
+++ Breakout/script.js
@@ -48,3 +48,4 @@ const user = document.createElement("div");
 user.classList.add("user");
 user.style.left = currentPosition[0] + "px";
 user.style.bottom = currentPosition[1] + "px";
+grid.appendChild(user);
