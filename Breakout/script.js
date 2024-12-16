e67b53f0c9b8826877c5db7705f85f8029981169
Frantisek Pelech <feromiro23@gmail.com>
2024-12-17 00:08:19 +0100

    Constant boardWidth

--- Breakout/script.js
+++ Breakout/script.js
@@ -1,6 +1,7 @@
 const grid = document.querySelector(".grid");
 const blockWidth = 100;
 const blockHeight = 20;
+const boardWidth = 600;
 const userStart = [230, 10];
 let currentPosition = userStart;
 
