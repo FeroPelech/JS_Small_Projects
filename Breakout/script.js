f56b36bc30a198ece44a46e91e6a0699cf38e72c
Frantisek Pelech <feromiro23@gmail.com>
2024-12-22 15:05:39 +0100

    Small correction

--- Breakout/script.js
+++ Breakout/script.js
@@ -4,7 +4,7 @@ const blockHeight = 20;
 const boardWidth = 600;
 const userStart = [230, 10];
 let currentPosition = userStart;
-const ballStart = [230, 40];
+const ballStart = [270, 40];
 let ballCurrentPosition = ballStart;
 
 class Block {
@@ -81,5 +81,5 @@ function moveUser(e) {
 const ball = document.createElement("div");
 ball.classList.add("ball");
 ball.style.left = ballCurrentPosition[0] + "px";
-ball.style.right = ballCurrentPosition[1] + "px";
+ball.style.bottom = ballCurrentPosition[1] + "px";
 grid.appendChild(ball);
