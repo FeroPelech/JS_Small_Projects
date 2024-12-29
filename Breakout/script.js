f6e4dc684064f444f9142924a1aeebf747b2f1e6
Frantisek Pelech <feromiro23@gmail.com>
2024-12-29 13:33:56 +0100

    If condition

--- Breakout/script.js
+++ Breakout/script.js
@@ -104,4 +104,9 @@ function checkForCollisions() {
   }
 }
 
-function changeDirection() {}
+function changeDirection() {
+  if (xDirection == 2 && yDirection == 2) {
+    yDirection = -2;
+    return;
+  }
+}
