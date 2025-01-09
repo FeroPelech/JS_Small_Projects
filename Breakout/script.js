0bf1fba1f9de9e61da60c934c71573f984da103f
Frantisek Pelech <feromiro23@gmail.com>
2025-01-09 19:04:19 +0100

    Call changeDirection function

--- Breakout/script.js
+++ Breakout/script.js
@@ -112,6 +112,7 @@ function checkForCollisions() {
       const allBlocks = Array.from(document.querySelectorAll(".block"));
       allBlocks[i].classList.remove("block");
       blocks.splice(i, 1);
+      changeDirection();
     }
   }
 
