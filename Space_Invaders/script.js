32115beccdf3b92b175ce57d6d0442d5b6dc9e9b
Frantisek Pelech <feromiro23@gmail.com>
2025-01-16 23:46:28 +0100

    For loop for creating squares

--- Space_Invaders/script.js
+++ Space_Invaders/script.js
@@ -1,2 +1,6 @@
 const container = document.querySelector(".container");
-console.log(container);
+
+for (let i = 0; i < 225; i++) {
+  const square = document.createElement("div");
+  container.appendChild(square);
+}
