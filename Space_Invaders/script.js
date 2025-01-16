02f4d5d894d8ab3bdd2c37d3e9ad55a3f2557d92
Frantisek Pelech <feromiro23@gmail.com>
2025-01-16 23:52:16 +0100

    Constant for squares

--- Space_Invaders/script.js
+++ Space_Invaders/script.js
@@ -1,4 +1,6 @@
 const container = document.querySelector(".container");
+const squares = Array.from(document.querySelectorAll(".container div"));
+console.log(squares);
 
 for (let i = 0; i < 225; i++) {
   const square = document.createElement("div");
