292bb5843892053c65217f5edf19521a6fad3961
Frantisek Pelech <feromiro23@gmail.com>
2025-01-16 23:53:56 +0100

    Constant array for alien invaders

--- Space_Invaders/script.js
+++ Space_Invaders/script.js
@@ -1,8 +1,12 @@
 const container = document.querySelector(".container");
 const squares = Array.from(document.querySelectorAll(".container div"));
-console.log(squares);
 
 for (let i = 0; i < 225; i++) {
   const square = document.createElement("div");
   container.appendChild(square);
 }
+
+const alienInvader = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 30, 31,
+  32, 33, 34, 35, 36, 37, 38, 39,
+];
