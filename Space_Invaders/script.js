f9a533388afce367ade33401351b4c39a70ba687
Frantisek Pelech <feromiro23@gmail.com>
2025-01-21 23:52:50 +0100

    Correction

--- Space_Invaders/script.js
+++ Space_Invaders/script.js
@@ -44,5 +44,6 @@ document.addEventListener("keydown", moveShooter);
 
 function moveInvaders() {
   const leftEdge = alienInvaders[0] % width === 0;
-  const rightEdge = alienInvaders[alienInvaders.length - 1];
+  const rightEdge =
+    alienInvaders[alienInvaders.length - 1] % width === width - 1;
 }
