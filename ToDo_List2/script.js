c686b927ae218a6cd3d04d9ff9f066761de503e4
Frantisek Pelech <feromiro23@gmail.com>
2025-02-23 18:29:36 +0100

    Created and added button for inserting task

--- ToDo_List2/script.js
+++ ToDo_List2/script.js
@@ -3,11 +3,16 @@ const container = document.querySelector(".container");
 const newtask = document.createElement("div");
 
 const input = document.createElement("input");
+
+const btn = document.createElement("button");
 input.type = "text";
 input.placeholder = "  New task";
-console.log(input);
+console.log(btn);
 newtask.id = "newtask";
+btn.id = "push";
+btn.textContent = "Insert";
 container.appendChild(newtask);
 newtask.appendChild(input);
+newtask.appendChild(btn);
 console.log(newtask);
 console.log(container);
