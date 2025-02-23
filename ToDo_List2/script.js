ed184cd95671e6ef102f9f1f47f1fd46b2ca2b53
Frantisek Pelech <feromiro23@gmail.com>
2025-02-23 18:20:28 +0100

    Created input and added to newtask id.

--- ToDo_List2/script.js
+++ ToDo_List2/script.js
@@ -1,7 +1,13 @@
 const container = document.querySelector(".container");
 
 const newtask = document.createElement("div");
+
+const input = document.createElement("input");
+input.type = "text";
+input.placeholder = "  New task";
+console.log(input);
 newtask.id = "newtask";
 container.appendChild(newtask);
+newtask.appendChild(input);
 console.log(newtask);
 console.log(container);
