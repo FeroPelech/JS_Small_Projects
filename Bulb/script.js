5fdd1feaa7c446cbcf00e28e0cec929267c9a57c
Frantisek Pelech <feromiro23@gmail.com>
2025-03-06 17:18:22 +0100

    Added btn class to button element

--- Bulb/script.js
+++ Bulb/script.js
@@ -4,6 +4,7 @@ const image = document.createElement("img");
 const btn = document.createElement("button");
 
 bulb.classList.add("bulb");
+btn.classList.add("btn");
 image.classList.add("image");
 image.src = "./Bulbs/BulbOn.png";
 image.alt = "Light";
