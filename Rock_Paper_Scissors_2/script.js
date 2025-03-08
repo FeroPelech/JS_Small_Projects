29d47f93aeb9de17a4ac68fd6f41261679a8a896
Frantisek Pelech <feromiro23@gmail.com>
2025-03-08 13:15:41 +0100

    Constant Result_Field for new div element

--- Rock_Paper_Scissors_2/script.js
+++ Rock_Paper_Scissors_2/script.js
@@ -1,2 +1,4 @@
-const container = document.querySelector(".container");
-console.log(container);
+const Container = document.querySelector(".container");
+
+const Result_Field = document.createElement("div");
+console.log(Result_Field);
