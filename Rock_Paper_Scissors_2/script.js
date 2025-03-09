b26e189e7888b7d3accebb6c1c440e72d41ef844
Frantisek Pelech <feromiro23@gmail.com>
2025-03-09 11:54:05 +0100

    Html element parts together

--- Rock_Paper_Scissors_2/script.js
+++ Rock_Paper_Scissors_2/script.js
@@ -9,4 +9,8 @@ result_field.classList.add("result_field");
 result_images.classList.add("result_images");
 user_result.classList.add("user_result");
 
+container.appendChild(result_field);
+result_field.appendChild(result_images);
+result_images.appendChild(user_result);
+user_result.appendChild(image);
 console.log(image);
