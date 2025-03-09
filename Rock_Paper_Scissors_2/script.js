59933b306b42674193fd6e1d03e8c4dc4ce771f1
Frantisek Pelech <feromiro23@gmail.com>
2025-03-09 11:14:20 +0100

    added class to result_field constant

--- Rock_Paper_Scissors_2/script.js
+++ Rock_Paper_Scissors_2/script.js
@@ -4,4 +4,6 @@ const result_field = document.createElement("div");
 const result_images = document.createElement("div");
 const user_result = document.createElement("span");
 const image = document.createElement("img");
+
+result_field.classList.add("result_field");
 console.log(image);
