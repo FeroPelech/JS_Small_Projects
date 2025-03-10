a3ed94ffddaf08c12770fec8a425d83add5e41e5
Frantisek Pelech <feromiro23@gmail.com>
2025-03-10 10:07:01 +0100

    Constant cpu_result

--- Rock_Paper_Scissors_2/script.js
+++ Rock_Paper_Scissors_2/script.js
@@ -3,6 +3,7 @@ const container = document.querySelector(".container");
 const result_field = document.createElement("div");
 const result_images = document.createElement("div");
 const user_result = document.createElement("span");
+const cpu_result = document.createElement("cpu_result");
 const image = document.createElement("img");
 
 result_field.classList.add("result_field");
