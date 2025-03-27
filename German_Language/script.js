b915a6e69c33bd5d85a4c6bba09a6f77086c1399
Frantisek Pelech <feromiro23@gmail.com>
2025-03-28 00:31:56 +0100

    Something wrong...

--- German_Language/script.js
+++ German_Language/script.js
@@ -1,1 +1,8 @@
 const container = document.querySelector(".container");
+import { readFile } from "F:GitHubRepoJS_Small_ProjectsGerman_LanguagesrcSlovnikDE-Telo.txt";
+
+readFile("SlovnikDE-Telo.txt", (err, data) => {
+  if (err) throw err;
+
+  console.log(data.toString());
+});
