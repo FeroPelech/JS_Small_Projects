dc824b9309119dcfbf133298305cdefc3409ca94
Frantisek Pelech <feromiro23@gmail.com>
2025-05-09 19:54:45 +0200

    Listener for search button.

--- Dictionary/script.js
+++ Dictionary/script.js
@@ -2,3 +2,7 @@ const url = "https://api.dictionaryapi.dev/api/v2/entries/en/hello";
 const result = document.getElementById("result");
 const sound = document.getElementById("sound");
 const btn = document.getElementById("search-btn");
+
+btn.addEventListener("click", () => {
+  console.log("something");
+});
