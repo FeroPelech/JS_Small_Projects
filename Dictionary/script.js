c2b5c0e7da3f7bc30e0b9bd9e5273f30747a4a93
Frantisek Pelech <feromiro23@gmail.com>
2025-05-10 18:23:07 +0200

    Inseretd code from html to the script code

--- Dictionary/script.js
+++ Dictionary/script.js
@@ -9,6 +9,20 @@ btn.addEventListener("click", () => {
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      result.innerHTML = ``;
+      result.innerHTML = `
+                  <div class="word">
+                <h3>Sample</h3>
+                <button>
+                    <i class="fa-solid fa-volume-high"></i>
+                </button>
+            </div>
+            <div class="details">
+                <p>pos</p>
+                <p>/sample/</p>
+            </div>
+            <p class="word-meaning"> Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            <p class="word-example">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Praesentium, aliquam?</p>
+      `;
     });
 });
