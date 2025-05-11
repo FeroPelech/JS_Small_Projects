c41bcb12c12f16fff69a1e502d6d92c537c5979a
Frantisek Pelech <feromiro23@gmail.com>
2025-05-11 18:36:28 +0200

    aound attribute for audio

--- Dictionary/script.js
+++ Dictionary/script.js
@@ -23,5 +23,7 @@ btn.addEventListener("click", () => {
             <p class="word-meaning">${data[0].meanings[0].definitions[0].definition}</p>
             <p class="word-example">${data[0].meanings[0].definitions[0].example}</p>
       `;
+      sound.setAttribute("src", `${data[0].phonetics[0].audio}`);
+      console.log(sound);
     });
 });
