afae00671f69a59587a77f672843acf6d97cbc13
Frantisek Pelech <feromiro23@gmail.com>
2025-05-21 18:24:50 +0200

    If condition and set roundWon on true

--- game/script.js
+++ game/script.js
@@ -56,6 +56,9 @@ function checkWinner() {
     if (cellA == "" || cellB == "" || cellC == "") {
       continue;
     }
+    if (cellA == cellB && cellB == cellC) {
+      roundWon = true;
+    }
   }
 }
 
