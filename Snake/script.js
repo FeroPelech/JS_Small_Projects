f82f5c76434c59417126894f5cac13e5dc42d3a2
Frantisek Pelech <feromiro23@gmail.com>
2025-05-25 12:30:45 +0200

    Constant for reset button

--- Snake/script.js
+++ Snake/script.js
@@ -1,3 +1,5 @@
 const gameBoard = document.querySelector("#gameBoard");
 const ctx = gameBoard.getContext("2d");
 const scoreText = document.querySelector("#scoreText");
+const resetBtn = document.querySelector(".resetBtn");
+console.log(resetBtn);
