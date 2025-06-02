19bd93ca937073959c12fdf5e684dcd3aa7fe09d
Frantisek Pelech <feromiro23@gmail.com>
2025-06-02 16:38:26 +0200

    Small correction 2

--- Snake/script.js
+++ Snake/script.js
@@ -1,6 +1,6 @@
 const gameBoard = document.querySelector("#gameBoard");
 const ctx = gameBoard.getContext("2d");
-const scoreText = document.querySelector("#scoreText");
+const scoreText = document.querySelector("#score");
 const resetBtn = document.querySelector(".resetBtn");
 const gameWidth = gameBoard.width;
 const gameHeight = gameBoard.height;
