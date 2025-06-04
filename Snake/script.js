3a9081f635b80224b1a1e094c5bbbd0c6ba626a2
Frantisek Pelech <feromiro23@gmail.com>
2025-06-04 03:58:33 +0200

    Score counter

--- Snake/script.js
+++ Snake/script.js
@@ -69,7 +69,8 @@ function drawFood() {
 function moveSnake() {
   const head = { x: snake[0].x + xVelocity, y: snake[0].y + yVelocity };
   snake.unshift(head);
-  if (false) {
+  if (snake[0].x == foodX && snake[0].y == foodY) {
+    score + 1;
   } else {
     snake.pop();
   }
