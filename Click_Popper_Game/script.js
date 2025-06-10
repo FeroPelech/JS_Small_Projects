dce105abcfc6e5be61c784efc438e253103a78f3
Frantisek Pelech <feromiro23@gmail.com>
2025-06-11 01:04:39 +0200

    Array from btn class

--- Click_Popper_Game/script.js
+++ Click_Popper_Game/script.js
@@ -4,3 +4,4 @@ const player = {};
 playArea.stats = document.querySelector(".stats");
 playArea.main = document.querySelector(".main");
 playArea.game = document.querySelector(".game");
+Array.from(document.querySelectorAll(".btn"));
