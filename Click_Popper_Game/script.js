7c6cf1692cad7e9eda4b0c8d2d3cb1b35dd1ace8
Frantisek Pelech <feromiro23@gmail.com>
2025-06-16 11:00:21 +0200

    Set player score in function start game on 0

--- Click_Popper_Game/script.js
+++ Click_Popper_Game/script.js
@@ -1,76 +1,18 @@
 const playArea = {};
 const player = {};
 let gameObj;
-// const data = {
-//   data: [
-//     {
-//       icon: "\u0026#8902;",
-//       value: 10,
-//     },
-//     {
-//       icon: "\u0026#10031;",
-//       value: 30,
-//     },
-//     {
-//       icon: "\u0026#10036;",
-//       value: 50,
-//     },
-//     {
-//       icon: "\u0026#10042;",
-//       value: 70,
-//     },
-//     {
-//       icon: "\u0026#10084;",
-//       value: 75,
-//     },
-//     {
-//       icon: "\u0026#9813;",
-//       value: 50,
-//     },
-//     {
-//       icon: "\u0026#9822;",
-//       value: 60,
-//     },
-//     {
-//       icon: "\u0026#9924;",
-//       value: 40,
-//     },
-//     {
-//       icon: "\u0026#9971;",
-//       value: 100,
-//     },
-//     {
-//       icon: "\u0026#9729;",
-//       value: -50,
-//     },
-//     {
-//       icon: "\u0026#9785;",
-//       value: -100,
-//     },
-//     {
-//       icon: "\u0026#9760;",
-//       value: -250,
-//     },
-//     {
-//       icon: "\u0026#9791;",
-//       value: -20,
-//     },
-//   ],
-// };
 
 playArea.stats = document.querySelector(".stats");
 playArea.main = document.querySelector(".main");
 playArea.game = document.querySelector(".game");
 playArea.btns = Array.from(document.querySelectorAll(".btn"));
 playArea.page = Array.from(document.querySelectorAll(".page"));
-console.log(playArea);
 document.addEventListener("DOMContentLoaded", getData);
 
 player.score = 0;
 player.items = 3;
 
 playArea.btns.forEach((e) => {
-  console.log(e);
   e.addEventListener("click", handleBtn);
 });
 
@@ -81,21 +23,18 @@ function getData() {
       return rep.json();
     })
     .then(function (data) {
-      console.log(data);
       gameObj = data.data;
-      console.log(gameObj);
     });
-  console.log("DOOOOm");
 }
 
 function handleBtn(e) {
-  console.log(e.target.classList.contains("newGame"));
   if (e.target.classList.contains("newGame")) {
-    console.log("Holla");
     startGame();
   }
 }
 
 function startGame() {
+  player.score = 0;
+
   console.log("start");
 }
