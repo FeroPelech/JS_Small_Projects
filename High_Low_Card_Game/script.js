a532eb2a8bdbc4d06d2cb8fe3057cab664acf9ee
Frantisek Pelech <feromiro23@gmail.com>
2025-06-27 16:23:24 +0200

    Constant for gamePlay

--- High_Low_Card_Game/script.js
+++ High_Low_Card_Game/script.js
@@ -1,2 +1,5 @@
 const message = document.querySelector(".message");
 const score = document.querySelector(".score");
+const btn = document.querySelectorAll("button");
+const gamePlay = document.querySelector(".gameplay");
+console.log(gamePlay);
