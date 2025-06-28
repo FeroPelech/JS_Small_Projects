a7af794a2748ee7cfdb7d73c1eadc3e69e80a88d
Frantisek Pelech <feromiro23@gmail.com>
2025-06-28 11:13:48 +0200

    Variable for currently card value

--- High_Low_Card_Game/script.js
+++ High_Low_Card_Game/script.js
@@ -2,4 +2,6 @@ const message = document.querySelector(".message");
 const score = document.querySelector(".score");
 const btn = document.querySelectorAll("button");
 const gamePlay = document.querySelector(".gameplay");
-console.log(gamePlay);
+
+let curCardValue = 0;
+console.log(curCardValue);
