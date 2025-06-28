de48793ccd175ea91b1374aad56c0aaaea9e7fa1
Frantisek Pelech <feromiro23@gmail.com>
2025-06-28 13:45:57 +0200

    Constant suits for specification of the cards

--- High_Low_Card_Game/script.js
+++ High_Low_Card_Game/script.js
@@ -7,6 +7,7 @@ let curCardValue = 0;
 let scoreValue = 0;
 let deck = [];
 const ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
-for (let i = 0; i < ranks.length; i++) {
-  console.log(ranks[i]);
+const suits = ["hearts", "diams", "clubs", "spades"];
+for (let i = 0; i < btn.length; i++) {
+  console.log(btn[i]);
 }
