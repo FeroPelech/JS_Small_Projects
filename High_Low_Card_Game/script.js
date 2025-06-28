3a864887a2b321fd3fb65da53648543d0f4e3635
Frantisek Pelech <feromiro23@gmail.com>
2025-06-28 11:18:33 +0200

    Constant ranks for cards values

--- High_Low_Card_Game/script.js
+++ High_Low_Card_Game/script.js
@@ -6,4 +6,7 @@ const gamePlay = document.querySelector(".gameplay");
 let curCardValue = 0;
 let scoreValue = 0;
 let deck = [];
-console.log(curCardValue);
+const ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+for (let i = 0; i < ranks.length; i++) {
+  console.log(ranks[i]);
+}
