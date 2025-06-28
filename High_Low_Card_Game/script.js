f3157f68e6709ec268f3e10bb30c63a071bcbe08
Frantisek Pelech <feromiro23@gmail.com>
2025-06-28 13:50:52 +0200

    Listener for buttons

--- High_Low_Card_Game/script.js
+++ High_Low_Card_Game/script.js
@@ -8,7 +8,10 @@ let scoreValue = 0;
 let deck = [];
 const ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 const suits = ["hearts", "diams", "clubs", "spades"];
-for (let i = 0; i < btn.length; i++) {}
+
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", playGame);
+}
 
 function playGame() {
   console.log("Ola");
