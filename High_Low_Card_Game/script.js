138b222d36396bd1b1a7811760750fcc5d8c73a8
Frantisek Pelech <feromiro23@gmail.com>
2025-06-28 13:48:37 +0200

    Function declaration playGame

--- High_Low_Card_Game/script.js
+++ High_Low_Card_Game/script.js
@@ -8,6 +8,8 @@ let scoreValue = 0;
 let deck = [];
 const ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 const suits = ["hearts", "diams", "clubs", "spades"];
-for (let i = 0; i < btn.length; i++) {
-  console.log(btn[i]);
+for (let i = 0; i < btn.length; i++) {}
+
+function playGame() {
+  console.log("Ola");
 }
