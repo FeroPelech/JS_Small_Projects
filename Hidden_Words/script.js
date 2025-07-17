c115be41b1e8ba8083a7b7fc238da2239db65e67
Frantisek Pelech <feromiro23@gmail.com>
2025-07-18 01:51:09 +0200

    Added to new div class message

--- Hidden_Words/script.js
+++ Hidden_Words/script.js
@@ -3,4 +3,5 @@ window.addEventListener("load", init);
 function init() {
   console.log("init function work");
   let div = document.createElement("div");
+  div.setAttribute("class", "message");
 }
