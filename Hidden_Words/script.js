cf70b21352ef6aa3789944b7e6a88cad07e8791f
Frantisek Pelech <feromiro23@gmail.com>
2025-07-18 01:53:24 +0200

    Added new div to the html document

--- Hidden_Words/script.js
+++ Hidden_Words/script.js
@@ -4,5 +4,6 @@ function init() {
   let div = document.createElement("div");
   div.setAttribute("class", "message");
   div.innerText = "Press start button";
+  document.body.appendChild(div);
   console.log(div);
 }
