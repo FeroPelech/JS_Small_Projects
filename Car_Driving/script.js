8d96f51374138acb83a9cdd76ea620667b01e6de
Frantisek Pelech <feromiro23@gmail.com>
2025-07-28 04:20:08 +0200

    Constant for gameArea

--- Car_Driving/script.js
+++ Car_Driving/script.js
@@ -1,3 +1,4 @@
 const score = document.querySelector(".score");
 const startScreen = document.querySelector(".startScreen");
+const gameArea = document.querySelector(".gameArea");
 console.log(startScreen);
