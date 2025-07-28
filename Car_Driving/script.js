8f3a71a221cf4efcc6f2431a2b61e2cd2c41d768
Frantisek Pelech <feromiro23@gmail.com>
2025-07-28 04:20:52 +0200

    Listener for startScreen

--- Car_Driving/script.js
+++ Car_Driving/script.js
@@ -1,4 +1,6 @@
 const score = document.querySelector(".score");
 const startScreen = document.querySelector(".startScreen");
 const gameArea = document.querySelector(".gameArea");
+
+startScreen.addEventListener("click", start);
 console.log(startScreen);
