aecc8980e1b2dd69df35df55c94c5c278b428732
Frantisek Pelech <feromiro23@gmail.com>
2025-07-29 02:20:20 +0200

    Listener for keydown

--- Car_Driving/script.js
+++ Car_Driving/script.js
@@ -3,6 +3,7 @@ const startScreen = document.querySelector(".startScreen");
 const gameArea = document.querySelector(".gameArea");
 
 startScreen.addEventListener("click", start);
+document.addEventListener("keydown", pressOn);
 
 function start() {
   console.log("Click");
