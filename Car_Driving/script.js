cbebf117780a19bc6b145b1a44d10b756affecf4
Frantisek Pelech <feromiro23@gmail.com>
2025-08-02 21:51:44 +0200

    For loop

--- Car_Driving/script.js
+++ Car_Driving/script.js
@@ -50,6 +50,7 @@ function start() {
   startScreen.classList.add("hide");
   gameArea.classList.remove("hide");
   player.start = true;
+  for (let x = 0; x < 5; x++) {}
   window.requestAnimationFrame(playGame);
   let car = document.createElement("div");
   car.innerText = "Car";
