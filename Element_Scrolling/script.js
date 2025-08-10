c1793554e74cb57d0cde92e1b10e0dc2297c0a68
Frantisek Pelech <feromiro23@gmail.com>
2025-08-10 22:04:17 +0200

    Constant output for output class

--- Element_Scrolling/script.js
+++ Element_Scrolling/script.js
@@ -2,3 +2,4 @@ const btn = document.querySelector("button");
 const cElement = document.getElementById("cElement");
 const sElement = document.getElementById("sElement");
 const speed = document.querySelector("input");
+const output = document.querySelector(".output");
