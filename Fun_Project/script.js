5137ba26a986d317760d8e443d8a4bdc636fb4e0
Frantisek Pelech <feromiro23@gmail.com>
2025-08-13 09:41:25 +0200

    Added left style

--- Fun_Project/script.js
+++ Fun_Project/script.js
@@ -6,6 +6,8 @@ rep.forEach((e, index) => {
   console.log(index);
   e.addEventListener("click", (e) => {
     console.log(index);
+    holder.obj = e.cloneNode(true);
+    console.log(holder.obj);
   });
   e.style.left = index * 100 + "px";
 });
