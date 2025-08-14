3b6e90f363f26e36627f6a44554aadb1a6987da5
Frantisek Pelech <feromiro23@gmail.com>
2025-08-14 21:03:58 +0200

    Style Top

--- Fun_Project/script.js
+++ Fun_Project/script.js
@@ -11,6 +11,7 @@ rep.forEach((e, index) => {
     holder.obj.classList.add("newb");
     holder.obj.style.backgroundColor = "green";
     holder.obj.style.left = ele.offsetLeft + "px";
+    holder.obj.style.top = ele.offsetTop + 200 + "px";
     console.log(holder.obj);
   });
   e.style.left = index * 100 + "px";
