2f8bc3b29e9ab424068cfc8c88fa8480a4e54b4a
Frantisek Pelech <feromiro23@gmail.com>
2025-08-17 23:46:21 +0200

    Small correction

--- Fun_Project/script.js
+++ Fun_Project/script.js
@@ -15,7 +15,7 @@ rep.forEach((ele, index) => {
     holder.obj.style.position = "absolute";
     holder.obj.style.backgroundColor = ranColor();
     holder.obj.style.left = ele.offsetLeft + "px";
-    holder.obj.style.top = ele.offsetTop + 200 + "px";
+    holder.obj.style.top = ele.offsetTop + 300 + "px";
 
     holder.obj.startX = ele.offsetLeft;
     holder.obj.startY = ele.offsetTop;
