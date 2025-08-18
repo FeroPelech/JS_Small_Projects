4e199ec5fbb0d23e679722b49c7c5594565a7b5c
Frantisek Pelech <feromiro23@gmail.com>
2025-08-18 03:24:14 +0200

    Variable score set on 0

--- Click_Counter/script.js
+++ Click_Counter/script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector(".container");
 const btn = document.querySelector(".btn");
 const p = document.querySelector(".text_note");
+let score = 0;
 
 btn.addEventListener("click", () => {
   console.log("Click");
