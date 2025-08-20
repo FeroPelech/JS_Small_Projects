77dae11d7339b983a892eeab1611a57ea0fffa52
Frantisek Pelech <feromiro23@gmail.com>
2025-08-20 10:06:13 +0200

    Close constant for close class

--- PopUp/script.js
+++ PopUp/script.js
@@ -1,5 +1,6 @@
 const output = document.querySelector(".output");
 const show = document.querySelector(".show");
+const close = document.querySelectorAll(".close");
 const images = document.querySelectorAll(".pop img");
 images.forEach((ele) => {
   ele.addEventListener("click", popImage);
