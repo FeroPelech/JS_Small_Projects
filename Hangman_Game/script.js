fecaa1809401ea10f564726368c3486c931007b1
Frantisek Pelech <feromiro23@gmail.com>
2025-08-27 21:12:53 +0200

    Constant for myWords array

--- Hangman_Game/script.js
+++ Hangman_Game/script.js
@@ -2,3 +2,4 @@ const msg = document.querySelector(".message");
 const output1 = document.querySelector(".output1");
 const output2 = document.querySelector(".output2");
 const btn = document.querySelector("button");
+const myWords = ["javascript", "course", "laurence"];
