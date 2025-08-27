4d35099ffa4f12b35b4b550a0facfb61ae6e4d44
Frantisek Pelech <feromiro23@gmail.com>
2025-08-27 21:10:09 +0200

    Constant for button class

--- Hangman_Game/script.js
+++ Hangman_Game/script.js
@@ -1,3 +1,4 @@
 const msg = document.querySelector(".message");
 const output1 = document.querySelector(".output1");
 const output2 = document.querySelector(".output2");
+const btn = document.querySelector("button");
