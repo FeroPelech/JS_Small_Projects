ca14cfd8ed88ad6aec76c4e4cde453bd24bf3b58
Frantisek Pelech <feromiro23@gmail.com>
2025-08-27 21:14:34 +0200

    Button listener

--- Hangman_Game/script.js
+++ Hangman_Game/script.js
@@ -4,3 +4,7 @@ const output2 = document.querySelector(".output2");
 const btn = document.querySelector("button");
 const myWords = ["javascript", "course", "laurence"];
 let player = {};
+
+btn.addEventListener("click", () => {
+  console.log("click");
+});
