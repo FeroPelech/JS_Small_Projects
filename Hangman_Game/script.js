3e9780978905ce48fee81c2535cc0d4547e43e7c
Frantisek Pelech <feromiro23@gmail.com>
2025-09-01 06:43:14 +0200

    Created new constant array for testig

--- Hangman_Game/script.js
+++ Hangman_Game/script.js
@@ -2,7 +2,8 @@ const msg = document.querySelector(".message");
 const output1 = document.querySelector(".output1");
 const output2 = document.querySelector(".output2");
 const btn = document.querySelector("button");
-const myWords = ["javascript", "course", "laurence"];
+// const myWords = ["javascript", "course", "laurence"];
+const myWords = ["aa", "bb", "cc"];
 let player = {};
 
 btn.addEventListener("click", () => {
