0378dbd0d8ee39040ab1c9716ecbee364818d958
Frantisek Pelech <feromiro23@gmail.com>
2025-09-04 09:59:36 +0200

    Call makeClick function

--- Modal_PopUp/script.js
+++ Modal_PopUp/script.js
@@ -1,6 +1,9 @@
 const btn = document.querySelectorAll(".modal");
 btn.forEach((e) => {
   console.log(e);
+  makeClick(e);
 });
 
-function makeClick(el) {}
+function makeClick(el) {
+  console.log("func");
+}
