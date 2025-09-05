95f818e2d87f6a0894f24392d9ac9f65a4b82f78
Frantisek Pelech <feromiro23@gmail.com>
2025-09-05 07:44:13 +0200

    Deleted console logs

--- Modal_PopUp/script.js
+++ Modal_PopUp/script.js
@@ -2,13 +2,11 @@ const btn = document.querySelectorAll(".modal");
 const modalWrapper = document.querySelector(".modalWrapper");
 const body = document.querySelector("body");
 btn.forEach((e) => {
-  console.log(e);
   makeClick(e);
 });
 
 function makeClick(el) {
   el.addEventListener("click", () => {
-    console.log("func");
     modalWrapper.classList.add("showModal");
   });
 }
