5057b63ff9625f14fccba69fde6e2b124e11de9e
Frantisek Pelech <feromiro23@gmail.com>
2025-09-08 03:59:08 +0200

    Copy images 2 times more

--- Slide_Show/script.js
+++ Slide_Show/script.js
@@ -5,4 +5,12 @@ const myImages = [
     img: "https://images.pexels.com/photos/206369/pexels-photo-206369.jpeg",
     caption: "Hello World Image 1",
   },
+  {
+    img: "https://images.pexels.com/photos/206369/pexels-photo-206369.jpeg",
+    caption: "Hello World Image 2",
+  },
+  {
+    img: "https://images.pexels.com/photos/206369/pexels-photo-206369.jpeg",
+    caption: "Hello World Image 3",
+  },
 ];
