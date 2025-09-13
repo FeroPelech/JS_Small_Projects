5b57e2b0ba6fb67a78d97a035c15bb1abf8222ac
Frantisek Pelech <feromiro23@gmail.com>
2025-09-13 02:41:03 +0200

    Stars value set in index plus 1

--- Rating_Project/script.js
+++ Rating_Project/script.js
@@ -1,3 +1,5 @@
 const stars = document.querySelectorAll(".star");
 const output = document.querySelector(".output");
-for (let i = 0; i < stars.length; i++) {}
+for (let i = 0; i < stars.length; i++) {
+  stars[i].starValue = i++;
+}
