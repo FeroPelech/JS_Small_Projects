ab598490f77c99e022089927d52a0eeadcd67499
Frantisek Pelech <feromiro23@gmail.com>
2025-09-13 03:18:18 +0200

    Small correction

--- Rating_Project/script.js
+++ Rating_Project/script.js
@@ -3,7 +3,9 @@ const output = document.querySelector(".output");
 for (let i = 0; i < stars.length; i++) {
   stars[i].starValue = i + 1;
   console.log(stars[i]);
-  stars[i].addEventListener("click", starRate);
+  ["mouseover", "mouseout", "click"].forEach((e) => {
+    stars[i].addEventListener(e, starRate);
+  });
 }
 
 function starRate(e) {
