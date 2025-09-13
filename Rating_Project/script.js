46553279f8f2b180223abf2d72f4cda11d4d1f13
Frantisek Pelech <feromiro23@gmail.com>
2025-09-13 03:02:14 +0200

    Listener for stars with index i

--- Rating_Project/script.js
+++ Rating_Project/script.js
@@ -1,6 +1,7 @@
 const stars = document.querySelectorAll(".star");
 const output = document.querySelector(".output");
 for (let i = 0; i < stars.length; i++) {
-  stars[i].starValue = i++;
+  stars[i].starValue = i + 1;
   console.log(stars[i]);
+  stars[i].addEventListener("click", starRate);
 }
