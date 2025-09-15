0e4fa718237a688905acb5db3f614d98b3717c19
Frantisek Pelech <feromiro23@gmail.com>
2025-09-15 03:59:30 +0200

    Deleted console log

--- Rating_Project/script.js
+++ Rating_Project/script.js
@@ -2,7 +2,6 @@ const stars = document.querySelectorAll(".star");
 const output = document.querySelector(".output");
 for (let i = 0; i < stars.length; i++) {
   stars[i].starValue = i + 1;
-  console.log(stars[i]);
   ["mouseover", "mouseout", "click"].forEach((e) => {
     stars[i].addEventListener(e, starRate);
   });
