bb049bbb8144e6e916e696633912e8ae03658f15
Frantisek Pelech <feromiro23@gmail.com>
2025-09-25 01:44:56 +0200

    Style for height

--- Plane_Bomber/script.js
+++ Plane_Bomber/script.js
@@ -32,6 +32,7 @@ function makeEnemy() {
   player.base = document.createElement("div");
   player.base.setAttribute("class", "base");
   player.base.style.width = Math.floor(Math.random() * 200) + 10 + "px";
+  player.base.style.height = Math.floor(Math.random() * 100) + 100 + "px";
 }
 
 function playGame() {
