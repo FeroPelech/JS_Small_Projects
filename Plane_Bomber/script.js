5100623cc6a0b4c969e6a26d076838bcd87cc90c
Frantisek Pelech <feromiro23@gmail.com>
2025-10-14 19:26:44 +0200

    Player level decrementation

--- Plane_Bomber/script.js
+++ Plane_Bomber/script.js
@@ -34,6 +34,7 @@ function start() {
 }
 
 function makeEnemy() {
+  player.level--;
   player.base = document.createElement("div");
   player.base.setAttribute("class", "base");
   player.base.style.width = Math.floor(Math.random() * 200) + 10 + "px";
