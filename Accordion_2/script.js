372d7c69c4309c7640952f1213bf3cd2f176c53b
Frantisek Pelech <feromiro23@gmail.com>
2025-10-16 15:24:16 +0200

    Small correction 1

--- Accordion_2/script.js
+++ Accordion_2/script.js
@@ -1,4 +1,8 @@
 const mainClass = document.querySelectorAll(".main");
 const contentClass = document.querySelectorAll(".content");
 
-for (let i = 0; i < mainClass.length; i++) {}
+for (let i = 0; i < mainClass.length; i++) {
+  mainClass[i].addEventListener("click", () => {
+    console.log(mainClass[i]);
+  });
+}
