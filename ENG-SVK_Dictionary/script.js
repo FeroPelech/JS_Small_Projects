fae200f37a5ba47f4871e7d853d8f5c00f4f950d
Frantisek Pelech <feromiro23@gmail.com>
2025-10-17 12:49:53 +0200

    Constant label for new label html element

--- ENG-SVK_Dictionary/script.js
+++ ENG-SVK_Dictionary/script.js
@@ -1,3 +1,9 @@
 const container = document.querySelector(".container");
 const menu = document.querySelector(".menu");
 console.log(menu);
+
+//Create menu content
+
+const label = document.createElement("label");
+
+console.log(label);
