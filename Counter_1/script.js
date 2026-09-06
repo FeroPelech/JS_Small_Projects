4ee07fc1cf94204857a9d16a8223bf651b5be437
FeroPelech <feromiro23@gmail.com>
2026-09-06 04:54:33 +0200

    Deleted console logs

--- Counter_1/script.js
+++ Counter_1/script.js
@@ -1,10 +1,8 @@
 const display = document.querySelector('.display')
 let value = Number(display.textContent)
-console.log(value)
 
 const decreaseBtn = document.querySelector('.minus')
 const increaseBtn = document.querySelector('.plus')
-console.log(decreaseBtn)
 
 decreaseBtn.addEventListener('click', () => {
   value = value - 1
