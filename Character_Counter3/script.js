1206e1f1007b93512d16797595215891f877dbfd
FeroPelech <feromiro23@gmail.com>
2026-09-10 16:40:18 +0200

    Created and called function newP

--- Character_Counter3/script.js
+++ Character_Counter3/script.js
@@ -2,5 +2,9 @@ const textArea = document.querySelector('textarea')
 const count = document.querySelector('.counter')
 
 textArea.addEventListener('input', (e) => {
-  console.log(e)
+  newP(e)
 })
+
+function newP(e) {
+  console.log(e)
+}
