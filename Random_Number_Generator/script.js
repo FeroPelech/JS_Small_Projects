8e06827161c594b68e524658f8485cb78897c7f6
FeroPelech <feromiro23@gmail.com>
2026-09-17 12:52:05 +0200

    Constant for second button

--- Random_Number_Generator/script.js
+++ Random_Number_Generator/script.js
@@ -1,3 +1,4 @@
 const firstValue = document.querySelector('.firstValue')
 const secondValue = document.querySelector('.secondValue')
 const text = document.querySelector('.result')
+const btn = document.querySelector('.btn')
